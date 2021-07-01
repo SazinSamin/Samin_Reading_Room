849858d963907caa4085c7dde785d0e0d80e0831
samin <sazinsamin@gmail.com>
2021-07-01 08:29:09 +0600

    Change name of "string to integer" programe to atoi.

---
+++ C/atoi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int atoi(char s[]){
+
+        int i, n = 0, sign, x = 0;
+        
+        for( i=0; isspace(s[i]); i++);
+        sign = (s[i] == '-') ? -1 : 1;
+        if(s[i] == '-' || s[i] == '+') i++;
+        for(; isdigit(s[i]); i++)                       //initialize loop with nothing
+                n = n * 10 + (s[i] - 48);
+        
+        return n * sign;
+}
+
+
+int main(){
+        printf("\nResult: %d", atoi("    021191011"));
+}
