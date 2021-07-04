cd90955f5d1a3f070ed1b83ffc66b998afbbb0df
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-07-04 09:04:04 +0600

    Update the two dimensional array values.
    As in first code the the two dimensional "month array" value begin with 0, which the design inspired by Denis Richie book.
    But I changed it to normal, where the "month array" begin with first month values,
    so, there also change in initial "i" value

--- C/days_months_coverter.c
+++ C/days_months_coverter.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 static char array[2][13] = {
-    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+    {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
 
 int day_to_month(int year, int yeardays, int *months, int *days);
 int month_to_days(int year, int month);
@@ -12,18 +12,18 @@ int main(){
         int month = 0, days= 0;
 
         printf("\nConvert months and days from days: ");
-        day_to_month(2012, 55, &month, &days);
+        day_to_month(2012, 85, &month, &days);
         printf("\nMonths: %d, days: %d", month, days);
 
         printf("\nCovert days from month: ");
-        printf("\nDays: %d", month_to_days(2012, 4));
+        printf("\nDays: %d", month_to_days(2012, 5));
 }
 
 int month_to_days(int year, int month){
         int leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
         int days = 0;
 
-        for (int i = 1; i <= month; i++){
+        for (int i = 0; i < month; i++){
                 days += array[leap][i];
         }
         return days;
@@ -32,7 +32,7 @@ int month_to_days(int year, int month){
 int day_to_month(int year, int yeardays, int *months, int *days){
         int leap = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
 
-        for (int i = 1; yeardays > array[leap][i]; i++){
+        for (int i = 0; yeardays > array[leap][i]; i++){
                 yeardays -= array[leap][i];
                 (*months)++;
         }
