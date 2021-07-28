9215dd6dde6087ecad2625c18e505094032b05eb
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-07-28 10:12:29 +0600

    Update scanf.cpp

--- Technique/scanf.cpp
+++ Technique/scanf.cpp
@@ -1,7 +1,10 @@
 //we use scanf to get input from the terminal or stdin.
 //Interesting fact about the scanf is that it return 
 //how much input you take
-//
+
+
+//***********   I have got a problem here, if we enter 2 elements, it does't stop taking input.
+
 
 #include<iostream>
 using namespace std;
