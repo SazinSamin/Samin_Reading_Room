2c454f82f405bbcea150633faa2561d27fbbd09a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-07-31 10:40:50 +0600

    Update ip_hash.cpp

--- Algorithm/Bit manipulation/ip_hash.cpp
+++ Algorithm/Bit manipulation/ip_hash.cpp
@@ -1,8 +1,15 @@
 //Every ip address is 4 bytes (32 bits). Has 4 sections, each section isolates by "."(dot)
 //Each section has 1 byte(8 bits) which range from 0-255.
-//Here in the code first we take each section decimal value. 
-//shifted that corresponding location, like 192 the first section has range from 24-32 bits.
-//So, we shifter that 24 bits and get that corresponding decimal value.
+//Here in the code we take each section decimal value. 
+//Consider a ip address 192.168.0.1
+//So now the most left section 192 which the first 8 bits.
+//And this bit is ahead of another 24 bit, so we shit it 24 for bit and get the corresponding decimal value.
+//We do this for all other value, and add them together and get the hash value...
+
+//         192      168        0         1
+//      11000000  10101000  00000000  00000001
+
+
 
 #include<iostream>
 using namespace std;
