5161703c25dd4a907ba48193380119983a1ee9a2
samin <sazinsamin@gmail.com>
2021-08-27 22:23:52 +0600

    Add range search & search folder.

--- Data Structure/Binary tree/Height.cpp
+++ Data Structure/Binary tree/Height.cpp
@@ -1,10 +1,7 @@
-int getHeight(struct Node* head){
+int Height(struct Node* head){
         if(head == NULL){
                 return 0;
         }
-
-        int left_h = getHeight(head->left);
-        int right_h = getHeight(head->right);
-
-        return 1 + max(left_h, right_h);
+        return 1 + max(Height(head->right), Height(head->left));
 }
+
