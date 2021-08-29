f95e043d673d247a734ad4e3cbecdee72dd9b4e9
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-08-29 21:02:20 +0600

    Update Tik Tok Toe.c

--- C/Tik Tok Toe.c
+++ C/Tik Tok Toe.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#define A 88
+#define B 48
+#define GAP 95
+
+
 void Display(int array[]);
 int MapKeyBoardInput(int data);
 int Math(int array[]);
@@ -10,7 +15,7 @@ int Check(int array[]);
 int main(){
         int array[9];
         for (int i = 0; i < 9; i++)
-                array[i] = 95;
+                array[i] = GAP;
         int i = 0;
         int player1 = 0;
         int player2 = 0;
@@ -18,36 +23,35 @@ int main(){
         Display(array);
 
         do{
-
+                
+                
                 printf("\n\n");
                 i += 1;
 
-                printf("\nPlayer A: ");
+                printf("\nPlayer X: ");
                 scanf("%d", &player1);
                 player1 = MapKeyBoardInput(player1);
-                if (array[player1] != 95){
+                if (array[player1] != GAP){
                         printf("\nPosition has already occupied\n Input again to another position: ");
                         scanf("%d", &player1);
                         player1 = MapKeyBoardInput(player1);
                 }
                 printf("\n");
-                array[player1] = 65;
+                array[player1] = A;
                 Display(array);
                 if (Check(array)) break;
 
 
-
-
-                printf("\n\nPlayer B: ");
+                printf("\n\nPlayer 0: ");
                 scanf("%d", &player2);
                 player2 = MapKeyBoardInput(player2);
-                if (array[player2] != 95){
+                if (array[player2] != GAP){
                         printf("\nPosition has already occupied\n Input again to another position: ");
                         scanf("%d", &player2);
                         player2 = MapKeyBoardInput(player2);
                 }
                 printf("\n");
-                array[player2] = 66;
+                array[player2] = B;
                 Display(array);
                 if (Check(array)) break;
 
@@ -58,46 +62,46 @@ int main(){
 int Math(int array[]){
 
         for (int i = 0; i < 9; i += 3){
-                if (array[i] == 65 && array[(i + 1)] == 65 && array[(i + 2)] == 65){
-                        return 65;
+                if (array[i] == A && array[(i + 1)] == A && array[(i + 2)] == A){
+                        return A;
                 }
-                if (array[i] == 66 && array[(i + 1)] == 66 && array[(i + 2)] == 66){
-                        return 66;
+                if (array[i] == B && array[(i + 1)] == B && array[(i + 2)] == B){
+                        return B;
                 }
         }
 
         int x = 0;
         for (int i = 0; i < 3; i++){
-                if (array[i] == 65 && array[(i + 3)] == 65 && array[(i + 6)] == 65){
-                        return 65;
+                if (array[i] == A && array[(i + 3)] == A && array[(i + 6)] == A){
+                        return A;
                 }
-                if (array[i] == 66 && array[(i + 3)] == 66 && array[(i + 6)] == 66){
-                        return 66;
+                if (array[i] == B && array[(i + 3)] == B && array[(i + 6)] == B){
+                        return B;
                 }
         }
 
         int i = 0;
-        if (array[i] == 65 && array[(i + 4)] == 65 && array[(i + 8)] == 65){
-                return 65;
+        if (array[i] == A && array[(i + 4)] == A && array[(i + 8)] == A){
+                return A;
         }
-        if (array[i] == 66 && array[(i + 4)] == 66 && array[(i + 8)] == 66){
-                return 66;
+        if (array[i] == B && array[(i + 4)] == B && array[(i + 8)] == B){
+                return B;
         }
 
         i = 2;
-        if (array[i] == 65 && array[(i + 2)] == 65 && array[(i + 4)] == 65){
-                return 65;
+        if (array[i] == A && array[(i + 2)] == A && array[(i + 4)] == A){
+                return A;
         }
 
-        if (array[i] == 66 && array[(i + 2)] == 66 && array[(i + 4)] == 66){
-                return 66;
+        if (array[i] == B && array[(i + 2)] == B && array[(i + 4)] == B){
+                return B;
         }
         return -1;
 }
 
 int AllPostionCheck(int array[]){
         for (int i = 0; i < 9; i++){
-                if (array[i] == 95){
+                if (array[i] == GAP){
                         return 0;
                 }
         }
@@ -106,20 +110,20 @@ int AllPostionCheck(int array[]){
 
 int Check(int array[]){
         if (AllPostionCheck(array)){
-                printf("\nMatch Draw \nGame End ---------------------------");
+                printf("\nGame End ---------------------------");
                 return 1;
         }
         int result = Math(array);
-        if (result == 65){
-                printf("\nPlayer A Win !!!!");
+        if (result == A){
+                printf("\nPlayer X Win");
                 return 1;
         }
-        else if (result == 66){
-                printf("\nPlayer B Win !!!");
+        else if (result == B){
+                printf("\nPlayer 0 Win");
                 return 1;
         }
         else{
-                printf("\nRunning----");
+                //printf("\nRunning----");
         }
         return 0;
 }
@@ -127,6 +131,7 @@ int Check(int array[]){
 
 
 void Display(int array[]){
+        system("clear");
         for (int i = 0; i < 9; i++){
                 if (i != 0 && i % 3 == 0){
                         printf("\n");
