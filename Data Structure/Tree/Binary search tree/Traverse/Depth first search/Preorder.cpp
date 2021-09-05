f5383583fc5354e165ae04558b031b5cabc0fb87
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-05 17:56:11 +0600

    Update Preorder.cpp

--- Data Structure/Tree/Binary search tree/Traverse/Depth first search/Preorder.cpp
+++ Data Structure/Tree/Binary search tree/Traverse/Depth first search/Preorder.cpp
@@ -1,3 +1,7 @@
+/*Preorder traversal is used to create a copy of the tree. Preorder traversal is also used to get prefix expression on of an 
+expression tree.*/
+
+
 void preorder(struct Node *head){
         if(head == NULL){
                 return;
