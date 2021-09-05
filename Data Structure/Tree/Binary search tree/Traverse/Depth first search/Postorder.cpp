e1e8a70bb05700022ad5fdb4dd3750f693c90a01
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-05 22:33:46 +0600

    Update Postorder.cpp

--- Data Structure/Tree/Binary search tree/Traverse/Depth first search/Postorder.cpp
+++ Data Structure/Tree/Binary search tree/Traverse/Depth first search/Postorder.cpp
@@ -1,4 +1,5 @@
 // Postorder traversal read the data from the leaf node & go up to the parent node. 
+// First visit all leaf node, and lastly visit the parent node
 // Postorder tree use for delete the complete tree. go to link for that.
 // https://github.com/SazinSamin/Samin_Reading_Room/tree/main/Data%20Structure/Tree/Binary%20search%20tree/Delete%20Full%20Tree
 
