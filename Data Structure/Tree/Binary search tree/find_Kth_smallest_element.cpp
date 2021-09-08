1caa96003940a1bf7aa159a3dd8dcdacb93aee2f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-08 13:34:06 +0600

    Update find_Kth_smallest_element.cpp

--- Data Structure/Tree/Binary search tree/find_Kth_smallest_element.cpp
+++ Data Structure/Tree/Binary search tree/find_Kth_smallest_element.cpp
@@ -17,7 +17,7 @@ int OrderStatistitcs(struct TreeNode* head, int k){
         
         
         
-        // here (s+1) represent the root node of tree which left subtree size we measured. 
+        // here (s+1) represent the next node of tree which left subtree size we have measured. 
         // Suppose if the left subtree size is 3, then we actullay have 3 nodes before the.
         // so this will compare with the 3 + 1 = 4th node.
         // if the Kth position macth with the any node, means we find the Kth node, return the value of the node.
