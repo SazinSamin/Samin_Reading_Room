6750156df8108853d85a715f99ceb37be60b7ed3
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-08 18:05:44 +0600

    Update rank_of_tree.cpp

--- Data Structure/Tree/Binary search tree/Order statistic/rank_of_tree.cpp
+++ Data Structure/Tree/Binary search tree/Order statistic/rank_of_tree.cpp
@@ -1,4 +1,17 @@
-// I donw
+// find the rank of element x in the tree,
+// means our given data index in the sorted list of elements of the tree.
+// we here use 0th based index.
+// Like this tree
+
+//                              5
+//                            /  \
+//                          3     8                          
+//                         / \     \
+//                        1   2     10
+
+// if we talk about the element whose value is 8 in this tree
+// then it's rank is 5, means its index is 5th in the sorted list of elements of this tree.
+
 
 
 int rank_of_tree(struct TreeNode* root, int data){
@@ -10,8 +23,7 @@ int rank_of_tree(struct TreeNode* root, int data){
                         rank += 1 + treeSize(root->left); 
                         root =  root->right;
                 }else{
-                        rank += treeSize(root->left);
-                        return rank;
+                        return rank + treeSize(root->left); 
                 }
         }
         return -1;
