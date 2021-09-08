db5ec3da73eeb545718a0638650cf9a8ec9423b1
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-08 12:22:49 +0600

    Update _treeSize.cpp

--- Data Structure/Tree/Binary search tree/_treeSize.cpp
+++ Data Structure/Tree/Binary search tree/_treeSize.cpp
@@ -3,7 +3,8 @@ int treeSize(struct TreeNode* head){
         // if head == NULL, means we are in the below the leaf node, where there is no node, so return 0.
         if(!head) return 0;
         
-        // recursively go for size of leaf subtree & right subtree, 
-        // we get the size of left subtree & right subtree, add two subtree sizes, and +1 for this current node.
+        // recursively go for sleft subtree & right subtree, 
+        // we get the size of left subtree & right subtree, add two subtree sizes, and +1 for current node.
+        // whose left & right subtree is called.
         return treeSize(head->left) + treeSize(head->right) + 1;
 }
