1d7b9bb331de153e975148483111ffc7c74dbb14
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-12 10:15:31 +0600

    Create input_from_text_File.cpp

---
+++ Technique/input_from_text_File.cpp
@@ -0,0 +1,5 @@
+// you can take input from any file here
+// value will automatically allocated to corresponding variable.
+// "file3.txt" here is an example file name.
+
+freopen("file3.txt", "r", stdin);
