ae5393d530a45ef8745af72b4e83419e0c7dcd5e
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-13 12:17:31 +0600

    Update string_pointer_traverse.cpp

--- C++/Pointer/string_pointer_traverse.cpp
+++ C++/Pointer/string_pointer_traverse.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 
 // as this in C or C++, when we pass an array(here is "char" array),
-// the array decays to pointer, means instead of copying the whole array & it's element, it just copy the it's first address of the array.
+// the array decays to pointer, means instead of copying the whole array & it's element, 
+// it just copy the it's first address of the array.
 // which is a pointer.
 // so, we decode that pointer, and can do pointer arithmatic to traverse tha char array here.
+// see Denis Riche book for that.
+
+
 void print(char *s){
         int i = 0;
         while(*s != '\0'){
