e397bc84d5126fa8f53a87025f58648c44523169
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-13 12:16:36 +0600

    Create string_pointer_traverse.cpp

---
+++ C++/Pointer/string_pointer_traverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+// as this in C or C++, when we pass an array(here is "char" array),
+// the array decays to pointer, means instead of copying the whole array & it's element, it just copy the it's first address of the array.
+// which is a pointer.
+// so, we decode that pointer, and can do pointer arithmatic to traverse tha char array here.
+void print(char *s){
+        int i = 0;
+        while(*s != '\0'){
+                cout<<*s++;
+        }
+}
+
+int main(){
+        char s[] = "Sazin Reshed Samin";
+        print(s);
+}
