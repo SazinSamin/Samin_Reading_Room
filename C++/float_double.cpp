8241241ad78ac63c43034e8aedde8ad810d4681d
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-13 10:15:56 +0600

    Create float_double.cpp

---
+++ C++/float_double.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+        // if you want float totally, which is 4 bytes long, then f in last of that value
+        // nor it will be be double automatically, 8 bytes long.
+        float a = 5.5f;
+}
