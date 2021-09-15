bd1f2e99e250a30e39236b765cdbef3dbd54947c
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-15 21:42:40 +0600

    Create Constructor.cpp

---
+++ C++/Object Oriented Programming/Constructor.cpp
@@ -0,0 +1,39 @@
+// https://www.youtube.com/watch?v=FXhALMsHwEY&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=25
+// https://www.geeksforgeeks.org/constructors-c/
+
+#include<iostream>
+using namespace std;
+
+class Player{
+
+        private:
+                // Private constructor, now can't instantiate a object from that class
+                //Player(int x, int y){}
+                //Player() = delete;
+
+        public:
+                int X;
+                int Y;
+
+        // Constructor declearation
+        Player(int x, int y);
+        // constructor definction can also be here.
+
+        void display(){
+                cout <<  X << " " << Y << endl;
+        }
+        
+};
+
+//constructor defintion
+Player::Player(int x, int y){
+        X = x;
+        Y = y;
+}
+
+
+
+int main(){
+        Player p1(10, 20);
+        p1.display();
+}
