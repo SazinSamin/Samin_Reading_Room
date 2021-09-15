38fcd97f7e640131256969795def4e9664b6c32a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-15 14:30:49 +0600

    Update Static.cpp

--- C++/Object Oriented Programming/Static.cpp
+++ C++/Object Oriented Programming/Static.cpp
@@ -1,17 +1,14 @@
 // https://www.geeksforgeeks.org/static-keyword-cpp/
 // https://www.youtube.com/watch?v=1QTZeeDL0bc
 //  same in other programming language like Dart -> https://docs.google.com/document/d/1tQRqFEWMXu675f-4fGTCZwy8n14ePPcquYPPpYqBDAI/edit
+// Static variable also have other functionality, see example -> https://github.com/SazinSamin/Samin_Reading_Room/blob/main/C++/functionality/static_variable.cpp
+
 
 /* A static method cannot access a class’s instance variables and instance methods, 
 because a static method can be called even when no objects of the class have been instantiated. 
 For the same reason, the this reference cannot be used in a static method. The this reference must refer to
 a specific object of the class, and when a static method is called, there might not be any objects of its class in memory.
 */
-
-
-
-
-
 
 
 
