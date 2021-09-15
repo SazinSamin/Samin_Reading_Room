8ee9d6f5200349bfcd4af71210ab1da59db77780
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-15 08:50:05 +0600

    Create static_variable.cpp

---
+++ C++/functionality/static_variable.cpp
@@ -0,0 +1,4 @@
+// https://www.geeksforgeeks.org/static-variables-in-c/
+
+/* Static variable in C++, have life time scope, so it always retain position, not destroyed by any function call or wipe out the
+  stack section of the memory*/
