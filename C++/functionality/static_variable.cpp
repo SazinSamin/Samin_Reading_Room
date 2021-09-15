2eede9116881c50301954b3ec9a05033f99aafa8
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-15 14:30:17 +0600

    Update static_variable.cpp

--- C++/functionality/static_variable.cpp
+++ C++/functionality/static_variable.cpp
@@ -2,3 +2,36 @@
 
 /* Static variable in C++, have life time scope, so it always retain position, not destroyed by any function call or wipe out the
   stack section of the memory*/
+
+
+#include<iostream>
+using namespace std;
+
+static int a = 20;
+
+void increament(){
+        ++a;
+}
+
+int main(){
+        cout << a << endl;
+        increament();
+        cout << a << endl;
+        increament();
+        cout << a << endl;
+        increament();
+        cout << a << endl;
+        increament();
+        cout << a << endl;
+        increament();
+}
+
+
+/* Output:
+20
+21
+22
+23
+24
+*/
+ 
