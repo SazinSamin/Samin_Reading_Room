e9944f6b304cd9f875dcd391c3e2efc15d1d0ab2
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-15 17:44:40 +0600

    Create enum.cpp

---
+++ C++/functionality/enum.cpp
@@ -0,0 +1,21 @@
+// https://www.youtube.com/watch?v=x55jfOd5PEE&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=24
+// https://www.geeksforgeeks.org/enumerated-types-or-enums-in-c/
+
+#include<iostream>
+using namespace std;
+
+// enum only take integer type
+// defaule start from 0
+// we can also define integer type
+// enum Gender:char{male, female};
+
+enum Gender{male, female};
+
+int main(){
+
+        // If we set datatype as enum name, it will just take the value defined inside the enum 
+        Gender x = male;
+        Gender y = female;
+        cout << x << endl;
+        cout << y << endl;
+}
