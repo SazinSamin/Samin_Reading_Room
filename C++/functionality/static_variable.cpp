7b06a0d0c67dd074d18c7218b39641fe55fd5c6b
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-15 14:52:19 +0600

    Update static_variable.cpp

--- C++/functionality/static_variable.cpp
+++ C++/functionality/static_variable.cpp
@@ -1,4 +1,6 @@
 // https://www.geeksforgeeks.org/static-variables-in-c/
+// static variable also initialized 0 by compiler if not explicitly initilized by programmer.
+
 
 /* Static variable in C++, have life time scope, so it always retain position, not destroyed by any function call or wipe out the
   stack section of the memory*/
@@ -7,31 +9,36 @@
 #include<iostream>
 using namespace std;
 
-static int a = 20;
+// Global static variable, which visiable to all over the file.
+static int a = 0;
 
-void increament(){
+void incrementGlobal(){
         ++a;
 }
 
-int main(){
-        cout << a << endl;
-        increament();
-        cout << a << endl;
-        increament();
-        cout << a << endl;
-        increament();
-        cout << a << endl;
-        increament();
-        cout << a << endl;
-        increament();
+int incrementLocal(){
+        // Local static variable, which visiable to only this function.
+        static int b = 0;
+        return ++b;
 }
 
-
+int main(){
+        cout << "Local static a: " << a << endl;
+        incrementGlobal();
+        cout << "Local static a: " << a << endl;
+        incrementGlobal();
+        cout << "Local static a: " << a << endl;
+        
+        cout << "Global static b: " << incrementLocal() << endl;
+        cout << "Global static b: " << incrementLocal() << endl;
+        cout << "Global static b: " << incrementLocal() << endl;
+}
 /* Output:
-20
-21
-22
-23
-24
+Local static a: 0
+Local static a: 1
+Local static a: 2
+Global static b: 1
+Global static b: 2
+Global static b: 3
 */
  
