a0af05c908b4f1feb90eeb478be17762f7e9e474
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-19 13:07:11 +0600

    Create variable_initialization.cpp

---
+++ C++/Style/variable_initialization.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+        char x1 = 's';
+        char x2{'s'};
+
+        char y1[] = {"Sazin"};
+        char y2[]{"Sazin"};
+
+        int z1 = 5;
+        int z2(5);
+}
