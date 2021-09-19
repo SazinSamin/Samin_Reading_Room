f50f57990f1dc7cf90a9f7e4b4b5d9b0b95ffc59
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-19 08:14:48 +0600

    Create 1117. Vitual_Function.cpp

---
+++ C++/Object Oriented Programming/1117. Vitual_Function.cpp
@@ -0,0 +1,6 @@
+// https://www.geeksforgeeks.org/object-oriented-programming-in-cpp/#inheritance
+// https://www.geeksforgeeks.org/multiple-inheritance-in-c/
+// https://www.youtube.com/watch?v=oIV2KchSyGQ&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=28
+
+
+// Commming soon
