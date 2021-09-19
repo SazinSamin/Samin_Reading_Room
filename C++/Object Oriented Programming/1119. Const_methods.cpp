64d6cdbeba298a12721b51bf43c8bbbb162ece44
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-19 17:58:25 +0600

    Create 1119. Const_methods.cpp

---
+++ C++/Object Oriented Programming/1119. Const_methods.cpp
@@ -0,0 +1,39 @@
+// https://www.geeksforgeeks.org/const-keyword-in-cpp/
+#include<iostream>
+using namespace std;
+
+
+// When a function is declared as const, it can be called on any type of object, const object as well 
+// as non-const objects.
+
+// Whenever an object is declared as const, it only can call the const function of the class. 
+// also it needs to be initialized at the time of declaration. 
+// However, the object initialization while declaring is possible only with the help of constructors.
+
+
+class Const{
+        public:
+                Const(int x){
+                        this->x = x;
+                }
+
+                int x;
+
+                int getValue() const{
+                        return x;
+                }
+
+                void setValue(int X){
+                        x = X;
+                }       
+};
+
+int main(){
+        Const c1(10);
+        c1.setValue(11);
+        cout << c1.getValue() << endl;
+        
+        const Const c2(12);
+        cout << c2.getValue() << endl;
+
+}
