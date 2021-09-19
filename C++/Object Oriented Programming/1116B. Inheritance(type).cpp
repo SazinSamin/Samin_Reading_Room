a85c099bdbd6e4c391810db4a1c6959561856413
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-19 08:21:22 +0600

    Create 1116B. Inheritance(type).cpp

---
+++ C++/Object Oriented Programming/1116B. Inheritance(type).cpp
@@ -0,0 +1,31 @@
+// https://www.geeksforgeeks.org/inheritance-in-c/
+
+#include<iostream>
+using namespace std;
+
+class Game{
+        
+}
+
+// single inheritance
+class Player : public Game{
+
+}
+
+// multilevel inheritance
+class GameResult : public Player{
+
+}
+
+// multiple inhertance
+class Detail : public Game, public Player{
+
+}
+
+
+
+
+int main(){
+
+
+}
