4babcc4406b0b687b9e3c9557af7c834f1598457
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-20 17:46:18 +0600

    Create mutable.cpp

---
+++ C++/functionality/mutable.cpp
@@ -0,0 +1,30 @@
+// as constant object can only call constant method of class
+// so, to somwhow change any variable inside the const method, we can
+// marks that variable as mutable.
+
+#include<iostream>
+using namespace std;
+
+class Player{
+        private:
+                int height;
+                mutable int weight;
+        public:
+                Player(int x, int y){
+                        height = x;
+                        weight = y;
+                }
+
+                void display() const{
+                        if(!weight){
+                                weight = 60;
+                        }
+                        cout << "heigh: " << height << " weight: " << weight << endl;
+                }
+};
+
+
+int main(){
+        const Player p1(75, 0);
+        p1.display();
+}
