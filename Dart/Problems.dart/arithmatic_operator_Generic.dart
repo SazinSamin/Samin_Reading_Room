03512eb563a565836ee15dbb9b5806497fc79de5
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-12 14:39:04 +0600

    Update arithmatic_operator_Generic.dart

--- Dart/Problems.dart/arithmatic_operator_Generic.dart
+++ Dart/Problems.dart/arithmatic_operator_Generic.dart
@@ -1,4 +1,8 @@
 // arithmatic operator doesn't work for Generic
+// solution: https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Dart/Info/arithmatic_operator_Generic.dart
+
+
+
 class Complex<T>{
   T real;
   T imag;
