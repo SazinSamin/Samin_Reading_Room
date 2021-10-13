3ee784f78c6e4555fd3af205c3907116b59c67bd
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-13 11:47:53 +0600

    Create Use_1DArray_instead_of_2DArray.cpp

---
+++ C++/Style/Use_1DArray_instead_of_2DArray.cpp
@@ -0,0 +1,48 @@
+// instead of declearing a two dimentional array, we can also do the same thing with one dimentional array.
+// beacuse in one dimentional array the memory are contiguous, so they have faster access.
+//  See this video last portion ->      https://www.youtube.com/watch?v=gNgUMA_Ur0U&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=64
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+        int n = 5;
+        int count = 0;
+        
+        // we have declear a two dimentional array, and put some value to that array in traditional way.
+        int array[n][n];
+        for(int i=0; i<n; i++){
+                for(int j=0; j<5; j++){
+                        array[i][j] = count++;
+                }
+        }
+
+        // accessing and printing that value to the console in regular way.
+         for(int i=0; i<n; i++){
+                for(int j=0; j<5; j++){
+                        cout << array[i][j] << endl;
+                }
+        }
+
+        cout << "\n Array2: " << endl;
+        count = 0;
+
+        // now we can put those value in one dimentional array in same way.
+        // declear one dimentional array with multification of rows and columns.
+        int array2[n*n];
+
+        // We can put the value in array by storing them in contigous location.
+        // when each row finish it jump into the next column by multiply the row with column length.
+        for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                        array2[(i*5) + j] = count++;
+                }
+        }
+
+        // Storing elements like this we can retrieve the elements in O(n) times.
+        for(int i=0; i<n*n; i++){
+                cout << array2[i] << endl;
+        }
+}
