bc048fc3c766c38450c36ef7d13261ab9c164e2a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-20 22:14:53 +0600

    Update variables.js

--- Javascript/Raw/variables.js
+++ Javascript/Raw/variables.js
@@ -5,3 +5,23 @@ var sallary = 400000;
 Name;
 Age;
 console.log(Age+sallary);
+
+
+
+// javascript "+" operator can do both string concatation and sumission arithmatic
+var stringNum = "100";
+var num1 = 5;
+var num2 = 10;
+
+var result1 = stringNum + num1;
+var result2 = num1 + num2;
+
+
+
+var color1 = new Array("Red", "Green", "Blue");
+var color2 = ["Purple", "Yellow", "Pink"];
+
+color1.push("Orange");
+color1.pop();
+
+color1.sort();
