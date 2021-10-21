668a178cdd9dac9f566b930729d84d14daf3074f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-21 21:33:40 +0600

    Create else_if.js

---
+++ Javascript/Raw/else_if.js
@@ -0,0 +1,11 @@
+var colors = ["red", "green", "yellow"];
+var selectedColor = colors[1];
+
+
+if(selectedColor === "red"){
+        console.log("Selected color is red");
+}else if(selectedColor === "green"){
+        console.log("Selected color is green");
+}else if(selectedColor === "yellow"){
+        console.log("Selected color is yellow");
+}
