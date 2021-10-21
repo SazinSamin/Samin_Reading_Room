73bdb12e95a8c2ccb1194bf6c52d8c2f9e2c045c
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-21 22:23:13 +0600

    Create 5_and(&&)_or(||)_operator.js

---
+++ Javascript/Raw/5_and(&&)_or(||)_operator.js
@@ -0,0 +1,26 @@
+// the "&&"(and) operator
+// if all the statement are true then selector become true
+// if any of the statement is false then selector become false
+var goodSleep = true;
+var goodMood = true;
+
+
+if(goodSleep && goodMood){
+        console.log("The day is awsome");
+}else{
+        console.log("Seems like a bad day");
+}
+
+
+// the "||"(or) operator
+// if any of the statement is true then selector become true
+// if all statement are false then selector become false
+var breakfast = true;
+var lunch = true;
+var dinner = false;
+
+if(breakfast || lunch || dinner){
+        console.log("Eat enough");
+}else{
+        console.log("I am starving");
+}
