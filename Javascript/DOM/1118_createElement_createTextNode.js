fed7bf966a0e281a9eea6e8b15be898419311c35
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-24 18:13:05 +0600

    Create 1118_createElement_createTextNode.js

---
+++ Javascript/DOM/1118_createElement_createTextNode.js
@@ -0,0 +1,40 @@
+var myTag = document.createElement('p');
+var myText = document.createTextNode("This is a new Paragrapgh");
+myTag.appendChild(myText);
+var myDiv = document.querySelector('body');
+myDiv.appendChild(myTag);
+
+
+
+
+/*<!-- helper html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <style>
+                .style_1{
+                        color: blue;
+                        background-color: yellow;
+                        font-size: 50px;
+                }
+        </style>
+
+</head>
+<body>
+        <h1 id="h1_1">Javascript</h1>
+        <div class="fast">
+                <p>United State</p>
+                <p>Japan</p>
+                <p>Norway</p>
+        </div>
+
+        <script src="test1.js"></script>
+
+
+</body>
+</html>*/
