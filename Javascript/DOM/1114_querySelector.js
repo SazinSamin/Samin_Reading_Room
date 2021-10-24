2667d73f4a2bafffe15e4aff7a84ac632552cd8e
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-24 10:55:18 +0600

    Create 1114_querySelector.js

---
+++ Javascript/DOM/1114_querySelector.js
@@ -0,0 +1,5 @@
+// One thing have to remember always about QuerySelector that
+// it always use the CSS syntax.
+
+var getElement = document.querySelector('#article_1 .info1');
+getElement.style.color = "green";
