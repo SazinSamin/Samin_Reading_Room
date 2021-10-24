71fda7202cbb0c8299a78fda76d9bc0db2500d66
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-24 20:14:20 +0600

    Create 1119_removeChild.js

---
+++ Javascript/DOM/1119_removeChild.js
@@ -0,0 +1,40 @@
+var myClass = document.querySelector('div');
+for(var i = 0; i < myClass.children.length; i++){
+        myClass.removeChild(myClass.children[i]);
+}
+
+
+
+
+
+/*<!-- helper html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <style>
+                .style_1{
+                        color: blue;
+                        background-color: yellow;
+                        font-size: 50px;
+                }
+        </style>
+
+</head>
+<body>
+        <h1 id="h1_1">Javascript</h1>
+        <div class="first">
+                <p>United State</p>
+                <p>Japan</p>
+                <p>Norway</p>
+        </div>
+
+        <script src="compare.js"></script>
+
+
+</body>
+</html>*/
