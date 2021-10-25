413ef07f16e680610c71dca793f24215e1a4cf11
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-25 09:48:19 +0600

    Create 1116_keydown.js

---
+++ Javascript/DOM/Events/1116_keydown.js
@@ -0,0 +1,30 @@
+var text = "";
+document.addEventListener('keydown', function(evt){
+        document.getElementById('Key').innerHTML = `Key: ${evt.key}`;
+});
+
+
+
+
+
+
+/*<!-- helper html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <style>
+        </style>
+
+</head>
+<body>
+        <h1 id="Key">Key</h1>
+        <div></div>
+
+        <script src="compare.js"></script>
+</body>
+</html>*/
