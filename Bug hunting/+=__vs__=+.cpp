2c03426c1b84abeed4e9f91c614b088788a42e2e
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-25 11:35:34 +0600

    Create +=__vs__=+.cpp

---
+++ Bug hunting/+=__vs__=+.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+       int a = 10;
+       int b = 10;
+  
+       // as we know += does, it add the value to it what is in right side we given
+       a += 10;
+       // a = a + 10;
+  
+  
+       // but what happened when we do =+, 
+       b =+ 10;
+
+       cout << a << endl;
+       cout << b << endl;
+}
