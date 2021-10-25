e42c18c55260736a017e83a904c4127e04279e92
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-25 06:39:44 +0600

    Create 1112_addEventListener.js

---
+++ Javascript/DOM/Events/1112_addEventListener.js
@@ -0,0 +1,39 @@
+var btn = document.querySelector('button');
+btn.addEventListener("click", function(){
+        alert("You have clicked on the button");
+})
+
+
+
+
+
+
+
+/*<!-- helper html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <style>
+                .style_1{
+                        color: blue;
+                        background-color: yellow;
+                        font-size: 50px;
+                }
+        </style>
+
+</head>
+<body>
+        <h1 id="h1_1">Javascript</h1>
+
+        <button>Click Me</button>
+
+        <script src="test1.js"></script>
+
+
+</body>
+</html>*/
