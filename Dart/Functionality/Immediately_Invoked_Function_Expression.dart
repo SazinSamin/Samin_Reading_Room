bfaa9b1c56b0dba8475633825eadb63102cd6b05
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-26 08:36:57 +0600

    Create  Immediately_Invoked_Function_Expression.dart

---
+++ Dart/Functionality/Immediately_Invoked_Function_Expression.dart
@@ -0,0 +1,10 @@
+// The Immediately_Invoked_Function_Expression avaible on Javascript, I also found this stuff also work in Dart.
+// https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Raw/10_Function/114_IIFE.js
+
+
+
+void main(){
+  ((){
+    print('Immediately Invoked Function Expression');
+  })();
+}
