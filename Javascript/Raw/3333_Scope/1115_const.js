63c9b1c293d9d9a1ec82bdab1e1a119c6936b96a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-26 10:15:41 +0600

    Create 1115_const.js

---
+++ Javascript/Raw/3333_Scope/1115_const.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+const x = 10;
+
+try{
+        x = 10;
+}catch(err){
+        console.log(err);
+}
+
+console.log(x);
