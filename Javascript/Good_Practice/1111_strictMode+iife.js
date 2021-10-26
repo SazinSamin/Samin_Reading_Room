4216eaef36476413c3b175265c833983af49e950
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-26 09:33:52 +0600

    Create 1111_strictMode+iife.js

---
+++ Javascript/Good_Practice/1111_strictMode+iife.js
@@ -0,0 +1,15 @@
+// IIFE: https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Raw/3333_Scope/1112_IIFE.js
+// strict Mode: https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Raw/3333_Scope/1113_strict_mode.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+// https://www.coursera.org/learn/javascript-basics/lecture/thJcX/strategy-2-use-strict
+
+
+// strict mode convert javascript silent error to throw erro
+// in strict mode, if you forget to use "var" in times of declearation of a variable, this will throw 
+// error immediatly.
+
+"use strict";
+
+var x = 10;
+var y = 20;
+console.log(x + y);
