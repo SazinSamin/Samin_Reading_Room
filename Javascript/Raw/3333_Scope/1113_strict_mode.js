34e127816bf2019a213605e701b2b203b7c412ab
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-26 09:32:55 +0600

    Create 1113_strict_mode.js

---
+++ Javascript/Raw/3333_Scope/1113_strict_mode.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+// https://www.coursera.org/learn/javascript-basics/lecture/thJcX/strategy-2-use-strict
+
+// strict mode convert javascript silent error to throw erro
+// in strict mode, if you forget to use "var" in times of declearation of a variable, this will throw 
+// error immediatly.
+"use strict";
+
+var x = 10;
+var y = 20;
+console.log(x + y);
