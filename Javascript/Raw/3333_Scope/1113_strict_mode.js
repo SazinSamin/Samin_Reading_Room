6cf147c10b3386cdbd4cc0a8ce18b696a7050b78
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-26 10:46:46 +0600

    Update 1113_strict_mode.js

--- Javascript/Raw/3333_Scope/1113_strict_mode.js
+++ Javascript/Raw/3333_Scope/1113_strict_mode.js
@@ -1,9 +1,11 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
 // https://www.coursera.org/learn/javascript-basics/lecture/thJcX/strategy-2-use-strict
 
-// strict mode convert javascript silent error to throw erro
-// in strict mode, if you forget to use "var" in times of declearation of a variable, this will throw 
-// error immediatly.
+// strict mode convert javascript silent error to throw error
+// in strict mode, if you forget to use "var" or "let" means the type of the variable
+// in times of declearation of a variable, this will throw error immediatly.
+
+
 "use strict";
 
 var x = 10;
