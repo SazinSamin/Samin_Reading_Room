4076bf569195433facb84e88b180ae136ac28959
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-10-31 16:59:07 +0600

    Create 10_Class.js

---
+++ Javascript/Raw/10_Class.js
@@ -0,0 +1,10 @@
+class Car{
+        constructor(name, year){
+                this.name = name;
+                this.year = year;
+        }
+}
+
+const car = new Car("BMW", 2014);
+console.log(car.name);
+console.log(car.year);
