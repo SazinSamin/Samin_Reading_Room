d0995c0dc9ee822c56138e3b966e42950b943b7d
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-11-20 18:19:00 +0600

    Update volatile.cpp

--- C++/functionality/volatile.cpp
+++ C++/functionality/volatile.cpp
@@ -1,2 +1,3 @@
 // https://www.educba.com/volatile-in-c/
 // https://stackoverflow.com/questions/246127/why-is-volatile-needed-in-c#
+// https://en.wikipedia.org/wiki/Volatile_(computer_programming)
