964fb964b3d31962c216476bd128b35e8125751a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-11-20 18:24:58 +0600

    Create random.c

---
+++ C/random.c
@@ -0,0 +1,12 @@
+// https://www.tutorialspoint.com/c_standard_library/c_function_rand.htm
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+        for(int i=0; i < 10; i++){
+                printf("%d\n", rand());
+        }
+}
