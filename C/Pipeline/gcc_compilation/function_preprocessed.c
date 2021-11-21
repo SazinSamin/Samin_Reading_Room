e2e79bf7ffe30680769445c99fd504f1f4911114
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-11-21 12:20:30 +0600

    Add files via upload

---
+++ C/Pipeline/gcc_compilation/function_preprocessed.c
@@ -0,0 +1,10 @@
+# 1 "function.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "function.c"
+int sum(int a, int b){
+     return a + b;
+}
---
+++ C/Pipeline/gcc_compilation/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "function.h"
+
+int main()
+{
+     printf("%d", sum(10, 20));
+     printf("\nCode executed successfully");
+    return 0;
+}
