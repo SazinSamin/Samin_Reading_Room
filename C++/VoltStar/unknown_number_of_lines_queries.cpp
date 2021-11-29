12a898db4990dfb608629ae0046d5d471c1a0b35
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-11-29 11:43:51 +0600

    Create unknown_number_of_lines_queries.cpp

---
+++ C++/VoltStar/unknown_number_of_lines_queries.cpp
@@ -0,0 +1,15 @@
+// C++ program to take unknown number input from the console.
+// this code doesn't work properly on vscode but fine with hackerrank editor.
+
+
+
+#include <iostream>
+#include<map>
+using namespace std;
+int main(){
+
+        string name;
+        while (cin >> name){
+                cout << name << endl;
+        }
+}
