cf0cb9f0a0a6311badc6dd18c37aeec62e0fb072
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-02 19:35:31 +0600

    Create 00043_string_template.js

---
+++ Javascript/Raw/00043_string_template.js
@@ -0,0 +1,9 @@
+// String literals use for runtime string manipulation
+// https://www.w3schools.com/js/js_string_templates.asp
+
+
+let price = 25;
+let amount = 10;
+
+const total = `Total expendature: ${price * amount}`;
+console.log(total);
