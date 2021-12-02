0059e434f2bbc678a2ca88161264bbb0419067aa
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-02 10:57:13 +0600

    Create 0002_operator.js

---
+++ Javascript/Raw/0002_operator.js
@@ -0,0 +1,17 @@
+// https://www.w3schools.com/js/js_operators.asp
+// https://www.w3schools.com/js/js_arithmetic.asp
+// https://www.w3schools.com/js/js_assignment.asp
+
+
+let x = 5;
+let y = 5;
+// equal value & type operator "===" , this will result true only when the variable type
+// value & type both are same
+(x === y ) ? console.log("True") : console.log("False");
+
+// normal comparison operator "==" only compare value not type
+
+// exponential
+// first value consider as value, second one as power.
+let exp = x ** y;
+console.log(exp);
