2ba4fcb1e4ff6bc15f987b8a8553a3566731f26d
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-07 22:52:16 +0600

    Create 0010_conversion.js

---
+++ Javascript/Raw/0010_conversion.js
@@ -0,0 +1,25 @@
+let PIString = "3.1416";
+let PINum = Number(PIString);
+console.log(PINum);
+
+let PIInt = parseInt(PIString);
+console.log(PIInt);     
+let PIFloat = parseFloat(PIString);
+console.log(PIFloat);
+
+
+let numeric = 3.1416;
+let numericString = String(numeric);
+console.log(numericString);
+console.log(numeric.toString());
+
+
+let exp = 3453445.656456;
+let convertExp = exp.toExponential();
+console.log(convertExp);
+
+let fixed = 34.3454236;
+let afterFixed = fixed.toFixed(3);
+console.log(afterFixed);
+
+
