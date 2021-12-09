9f8251fd0d70666c58d91f685eb984758c5760d7
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-09 22:29:45 +0600

    Update 1117_hoisting.js

--- Javascript/Raw/3333_Scope/1117_hoisting.js
+++ Javascript/Raw/3333_Scope/1117_hoisting.js
@@ -1,5 +1,9 @@
 // https://www.digitalocean.com/community/tutorials/understanding-hoisting-in-javascript
 
+// Hoisting is (to many developers) an unknown or overlooked behavior of JavaScript.
+// If a developer doesn't understand hoisting, programs may contain bugs (errors).
+// To avoid bugs, always declare all variables at the beginning of every scope.
+// Since this is how JavaScript interprets the code, it is always a good rule.
 
 // JavaScript only hoists declarations, not initializations.
 
