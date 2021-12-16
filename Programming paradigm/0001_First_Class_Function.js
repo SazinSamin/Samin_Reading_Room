87fe27127c5af329d73a798883355ac1fe219616
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-16 11:17:59 +0600

    Create 0001_First_Class_Function.js

---
+++ Programming paradigm/0001_First_Class_Function.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/en-US/docs/Glossary/First-class_Function
+// First - class Function
+// A programming language is said to have First - class functions when functions in that language are treated like any other variable.For example, in such a language, a function can be passed as an argument to other functions, can be returned by another function and can be assigned as a value to a variable.
+
+
+function askUserName(){
+        return prompt("What is your name");
+}
+
+// here we receive function as argument
+const foo = function(name){
+        // we can also return function which called "higher order funciton"
+        return function(){
+                console.log(`Assalamu Alaikum ${name()}`);
+        }
+}
+
+
+// here we send function as argument called "callback funciton"
+foo(askUserName)();
