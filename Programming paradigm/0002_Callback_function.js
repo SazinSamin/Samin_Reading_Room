2d477b5a53f9db62516b54533c40d69f44b8bc99
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-16 11:27:11 +0600

    Create 0002_Callback_function.js

---
+++ Programming paradigm/0002_Callback_function.js
@@ -0,0 +1,16 @@
+// https://developer.mozilla.org/en-US/docs/Glossary/Callback_function
+// A callback function is a function passed into another function as an argument, 
+// which is then invoked inside the outer function to complete some kind of routine or action.
+
+
+function askUserName(){
+        return prompt("What is your name: ");
+}
+
+// receive funciton as argument
+function greeting(username){
+        console.log(`Congratulation ${username()}`);
+}
+
+// pass funciton as argument
+greeting(askUserName);
