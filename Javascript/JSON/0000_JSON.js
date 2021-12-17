a180dcdd3926d2240c00a11f995ceb76358337bc
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-17 07:30:19 +0600

    Create 0000_JSON.js

---
+++ Javascript/JSON/0000_JSON.js
@@ -0,0 +1,13 @@
+// https://www.w3schools.com/js/js_json.asp
+//  data is in always name, value pair
+//  data is sperated by commas
+// JSON name require double quote, JS doesn't need that
+// "{}" curly braces use for object 
+// "[]" square brackets use for array
+
+let obj1 = {"employee":[
+        {"fistName": "Sazin", "lastName": "Samin"},
+        {"firstName": "Abeed", "lastName": "Hossain"},
+]}
+
+
