20683f6b8e16358d0092151ec1ada4a24e0fbca7
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-18 08:58:49 +0600

    Create 0002_floating_point_precesion.js

---
+++ Javascript/Silly/0002_floating_point_precesion.js
@@ -0,0 +1,10 @@
+let a = 0.1;
+let b = 0.2;
+
+// this will result 0.30000000000000004
+let result = a + b;
+console.log(result);
+
+// for precise & rounded value
+let roundingResult = (a * 10 + b * 10) / 10;
+console.log(roundingResult);
