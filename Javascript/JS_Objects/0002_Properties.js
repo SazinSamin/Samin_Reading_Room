71f4eaa2317f8e39da7331de10ce5f2bc5a2dd58
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-21 19:55:35 +0600

    Update 0002_Properties.js

--- Javascript/JS_Objects/0002_Properties.js
+++ Javascript/JS_Objects/0002_Properties.js
@@ -5,13 +5,19 @@ const obj1 = {
 };
 
 // for..in loop loops over the js object property
-for(let x in obj1){
+for (let x in obj1) {
         console.log(obj1[x]);
 }
 
 // adding new property to the object
 obj1.occupation = "Software engineer";
 console.log(obj1);
+
+// adding new property using "defineProperty" object
+Object.defineProperty(obj1, "jobDescription", {
+        value: "software engineer"
+});
+
 
 // deletation of a property
 delete obj1.age;
@@ -23,7 +29,7 @@ const obj2 = {
         firstName: "Sazin",
         lastName: "Samin",
         age: 24,
-        location:{
+        location: {
                 homeTown: "Jhenaidah",
                 Division: "Khulna"
         }
