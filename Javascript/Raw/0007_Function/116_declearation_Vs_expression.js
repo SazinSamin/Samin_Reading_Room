4840ff9f773faaef3dede697266b9c05f82709eb
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-23 10:36:36 +0600

    Create 116_declearation_Vs_expression.js

---
+++ Javascript/Raw/0007_Function/116_declearation_Vs_expression.js
@@ -0,0 +1,9 @@
+// https://www.w3schools.com/js/js_function_definition.asp
+
+// function declearation
+function myFunc1(parameters) {
+
+}
+
+// function expression
+const myFunc2 = function () { console.log("Javascipt")};
