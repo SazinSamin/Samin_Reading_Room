85ec03451b02d686e0a806d135e88b4a4f0c0059
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-24 19:20:04 +0600

    Update 002_example.dart

--- Miscellaneous/Closure/002_example.dart
+++ Miscellaneous/Closure/002_example.dart
@@ -1,1 +1,14 @@
-
+Function counter() {
+  int count = 0;
+  int increment() {
+    return ++count;
+  }
+  return increment;
+}
+void main(){
+  Function count = counter();
+  print(count());
+  print(count());
+  print(count());
+  print(count());
+}
