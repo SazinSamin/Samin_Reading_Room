9f0375ee6f4d22e9989ae2e5b10f81a3bdcec43d
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-25 11:37:31 +0600

    Create 0002_Inhertance.js

---
+++ Javascript/OOP/0002_Inhertance.js
@@ -0,0 +1,24 @@
+class Person{
+        constructor(name, age) {
+                this.name = name;
+                this.age = age;
+        }
+
+        display() {
+                console.log(`${this.name}, ${this.age}`);
+        }
+}
+
+// use "extends" keyword for new class to inherit method
+class Student extends Person{
+        constructor(name, age, id) {
+                // The super() method refers to the parent class.
+                // By calling the super() method in the constructor method, we call the pare    nt's
+                // constructor method and gets access to the parent's properties and methods.
+                super(name, age);
+                this.id = id;
+        }
+}
+
+const s1 = new Student("Sazin", 24, 191011);
+s1.display();
