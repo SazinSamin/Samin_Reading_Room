7b92177ce72efd54d1d753422d23bf58bc9ffff4
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-25 21:43:14 +0600

    Create 0005_ClassMethod_VS_StaticMethod.js

---
+++ Javascript/OOP/0005_ClassMethod_VS_StaticMethod.js
@@ -0,0 +1,37 @@
+// https://www.samanthaming.com/tidbits/39-es6-classes/
+
+
+class Person{
+        constructor(name, age) {
+                // instance variable
+                this.name = name;
+                this.age = age;
+        }
+
+        // instance method
+        display() {
+                return this.name + " " + this.age;
+        }
+
+        // class variable
+        static status = "";
+        //  class method
+        static showStatus() {
+                console.log(this.status);
+        }
+};
+
+// instance memeber initialization
+const instance = new Person("Sazin", 24);
+const instanceMethod = instance.display();
+console.log(instanceMethod);
+
+const instanceMethod2 = new Person("Sazin", 24).display();
+console.log(instanceMethod2);
+
+// class variable initialization
+Person.status = "Software engineer";
+// class method invocation.
+Person.showStatus();
+
+
