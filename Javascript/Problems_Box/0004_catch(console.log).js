0a024b953df7949eae5eb3c2f294a1afe4ccfcd4
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-30 09:51:20 +0600

    Create 0004_catch(console.log).js

---
+++ Javascript/Problems_Box/0004_catch(console.log).js
@@ -0,0 +1,11 @@
+// as I found two different same code
+// i can't clearly understand how they work in behind.
+
+catch (err => {
+  console.log(err);
+})
+
+
+// ***************************
+
+catch(console.log);
