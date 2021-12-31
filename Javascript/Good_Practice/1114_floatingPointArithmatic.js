1472db1fe8580bc67ebb6068dfde527969736f6b
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-31 19:39:52 +0600

    Create 1114_floatingPointArithmatic.js

---
+++ Javascript/Good_Practice/1114_floatingPointArithmatic.js
@@ -0,0 +1,6 @@
+// https://modernweb.com/what-every-javascript-developer-should-know-about-floating-points/
+
+
+let a = 0.1;
+let b = 0.2;
+console.log(a + b); // output will be 0.30000000000000004
