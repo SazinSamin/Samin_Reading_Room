fb065d10608494acca073472469f5118c218d4c1
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-03 11:33:20 +0600

    Update 0001_Kadane's.cpp

--- Algorithm/Dynamic Programming/Test/Kadane's_algorithm/0001_Kadane's.cpp
+++ Algorithm/Dynamic Programming/Test/Kadane's_algorithm/0001_Kadane's.cpp
@@ -1,6 +1,3 @@
-// https://www.youtube.com/watch?v=YxuK6A3SvTs&t=3s
-// https://en.wikipedia.org/wiki/Maximum_subarray_problem
-
 #include<iostream>
 #include<vector>
 #include<climits>
@@ -9,7 +6,12 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        
         int max_end = 0, max_sum = INT_MIN;
+        
+        // we can also get the subarray starting & ending value, 
+        //  comment out commnet statement and get the result.
+        // int start = 0, stop = 0;
 
         for(int i=0; i<nums.size(); i++) {
 
@@ -17,19 +19,22 @@ public:
 
             if(max_end < nums[i]) {
                 max_end = nums[i];
+                // start = nums[i];
             }
 
             if(max_sum < max_end) { 
                 max_sum = max_end;
+                // stop = nums[i];
             }
         }
+        // cout << start << " " << stop << endl;
         return max_sum;
     }
 
 };
 
 int main() {
-    vector<int> v = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    vector<int> v = {};
     Solution s = Solution();
     cout << s.maxSubArray(v) << endl;
 }
