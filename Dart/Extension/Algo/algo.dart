93334eda9e6f70a929ec532a9799f4773eaa9c48
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-03 23:13:11 +0600

    Update algo.dart

--- Dart/Extension/Algo/algo.dart
+++ Dart/Extension/Algo/algo.dart
@@ -1,31 +1,29 @@
 extension Algo on List {
-
   static const MIN_VAL = -9223372036854775808;
 
-  // return the sum of a subarray maximum value
-  // Based on Kadane's algorithm 
-  // https://github.com/SazinSamin/Samin_Reading_Room/tree/main/Algorithm/Dynamic%20Programming/Test/Kadane's_algorithm
-  
-  num maxSubArraySum() {
-    
-    num sum = 0, max = MIN_VAL;
-    List<dynamic> list = this;
-    int size = list.length;
+  /// return the [sum] of a subarray [maximum value]
+  /// Based on [Kadane's] algorithm
+  /// https://github.com/SazinSamin/Samin_Reading_Room/tree/main/Algorithm/Dynamic%20Programming/Test/Kadane's_algorithm
 
-    try{
-      for(int i=0; i<size; i++) {
-        sum += list[i];
-        if(sum > max) {
-          max = sum;
+  num maxSubArraySum() {
+    num _sum = 0, _max = MIN_VAL;
+    List<dynamic> list = this;
+    int _size = list.length;
+
+    try {
+      for (int i = 0; i < _size; i++) {
+        _sum += list[i];
+        if (_sum > _max) {
+          _max = _sum;
         }
-        if(sum < 0) {
-          sum = 0;
+        if (_sum < 0) {
+          _sum = 0;
         }
       }
-    }catch(e) {
+    } catch (e) {
       print(e);
       return -1;
     }
-    return max;
+    return _max;
   }
 }
