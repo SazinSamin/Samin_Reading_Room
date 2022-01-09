5d3442ce947aa5c34b4801854d140fc7493c6532
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-09 22:04:25 +0600

    Create 0001_reserve.cpp

---
+++ C++/Container/0002_Vector/0001_reserve.cpp
@@ -0,0 +1,21 @@
+// https://www.geeksforgeeks.org/using-stdvectorreserve-whenever-possible/
+
+// vector is dynamic allocating data structure.
+// but we can allocate space in first position like array.
+// so the requested space will allocated for the vector.
+// no dynamic allocation will happened before the space has consumed.
+// increase performace.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+        vector<int> v1;
+        v1.reserve(5);
+        v1 = {1, 2, 3, 4, 5};
+
+        for(auto it = v1.begin(); it != v1.end(); it++) {
+                cout << *it << endl;
+        }
+}
