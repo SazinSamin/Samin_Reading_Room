8d348824a0ea2d2d6b4a8527869df7648a639f17
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-17 11:12:56 +0600

    Update 0001_unorded_set.cpp

--- C++/Container/0001_unorded_set.cpp
+++ C++/Container/0001_unorded_set.cpp
@@ -1,24 +1,53 @@
 // https://www.geeksforgeeks.org/unordered_set-in-cpp-stl/
-// find operation
-// https://www.geeksforgeeks.org/unordered_set-find-function-in-c-stl/
+
 
 #include<iostream>
 #include<unordered_set>
 using namespace std;
 
 int main() {
-        unordered_set<int> set;
-        int array[4] = {1, 2, 3, 4};
-        for(int i=0; i<4; i++) {
-                set.insert(array[i]);
-        }
-  
-        // if find() operation found the element it return an iterator, else it return end of the set.
-        if(set.find(4) != set.end()) {
-                cout << "Element found" << endl;
-        } else {
-                cout << "Element not found" << endl;
+        unordered_set<int> set1;
+        for(int i=1; i <=20; i++) {
+                set1.insert(i);
         }
 
-        return 0;
+        // return iterator, point to the first element.
+        unordered_set<int>::iterator it;
+        it = set1.begin();
+        cout << *it << endl;
+
+        // last of set, not a element.
+        it = set1.end();
+
+        // find an element in set
+        if(set1.find(3) != set1.end())
+                cout << "Element found" << endl;
+        else
+                cout << "Not found" << endl;
+
+
+        // remove all elements
+        // set1.clear();
+
+        cout << "Backet count: " << set1.bucket_count() << endl;
+        // bucket_size(param), here param is bucket number, mean which bucket total elements size we want.
+        cout << "Num 3 Bucket total elements: " << set1.bucket_size(1) << endl;
+
+        // erase a specifiq element from bucket.
+        set1.erase(3);
+
+        // return size of set
+        cout << "size: " << set1.size() << endl;
+        
+        // return true & iterator if element already present or return false if not present
+        set1.emplace(5);
+
+        cout << "Max size:  " << set1.max_size() << endl;
+
+        cout << "Is empty " << set1.empty() << endl;
+
+        cout << "Load factor: " << set1.load_factor() << endl;
+        
+
+
 }
