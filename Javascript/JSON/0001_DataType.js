e10005db948208789df2eb4ab8644c7d3421c117
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-22 11:22:27 +0600

    Create 0001_DataType.js

---
+++ Javascript/JSON/0001_DataType.js
@@ -0,0 +1,10 @@
+// JSON data type can be string, number, array & object.
+// we can also keep function in JSON, but that's not preferable
+let json1 = `{
+        "name": "Abid",
+        "age": 23,
+        "Car": ["Ford", "Ferari", "Marcedes"],
+        "Occupation" : {"title":"Student", "Status": "True"}
+}`;
+
+console.log(json1);
