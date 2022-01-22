6b0ca3a95bc5d3357bb82a42c2c3c8e619cfabf0
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-22 22:45:46 +0600

    Create 003_MaxMin _from_Array.js

---
+++ Javascript/Silly/003_MaxMin _from_Array.js
@@ -0,0 +1,5 @@
+// https://medium.com/@vladbezden/how-to-get-min-or-max-of-an-array-in-javascript-1c264ec6e1aa
+
+const array = [1, 2, 3, 5];
+console.log(Math.max(...array));
+console.log(Math.min(...array));
