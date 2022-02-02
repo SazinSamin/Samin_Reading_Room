0f63e569591fb8d912000e2eb52fdaade80f3cdc
samin <sazinsamin@gmail.com>
2022-02-02 20:04:31 +0600

    Change name misselinous to Documentation.

---
+++ Documentation/Anonymous Function/02_example.dart
@@ -0,0 +1,8 @@
+void main () {
+  Function getSum = (int a, int b) {
+    return a + b;
+  };
+
+  int result = getSum(10, 20);
+  print(result);
+}
---
+++ Documentation/Closure/002_example.dart
@@ -0,0 +1,17 @@
+Function counter() {
+  int count = 0;
+  int increment() {
+    return ++count;
+  }
+  return increment;
+}
+
+
+
+void main(){
+  Function count = counter();
+  print(count());
+  print(count());
+  print(count());
+  print(count());
+}
