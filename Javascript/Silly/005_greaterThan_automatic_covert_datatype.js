e8872402c243f3e2fc982789d763a9c706ba114c
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-04 09:53:26 +0600

    Create 005_greaterThan_automatic_covert_datatype.js

---
+++ Javascript/Silly/005_greaterThan_automatic_covert_datatype.js
@@ -0,0 +1,9 @@
+// be carefull like the equality operator, the greater than operator will convert data types of values while comparing.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Greater_than_or_equal
+
+
+
+const num1 = 10;
+const string1 = "12";
+
+(string1 > num1) ? console.log("True") : console.log("False");
