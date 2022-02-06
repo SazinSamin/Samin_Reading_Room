672120b7ab87a1af48ec34db30c15e59ee0b769a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-06 10:19:23 +0600

    Create 006_randomNumber.js

---
+++ Javascript/Silly/006_randomNumber.js
@@ -0,0 +1,14 @@
+function randomFraction() {
+
+        // Only change code below this line
+        const testArray = [];
+        const unique = new Set();
+        for(let i = 0; i < 100; i++) {
+                console.log(parseInt(Math.random() * 10));
+        }
+        return unique;
+        // Only change code above this line
+}
+
+
+console.log(randomFraction());
