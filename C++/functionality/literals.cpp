e8e5d508beaf3e58b98ecfee6f0d48111bf212e8
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-07 09:23:11 +0600

    Create literals.cpp

---
+++ C++/functionality/literals.cpp
@@ -0,0 +1,16 @@
+// https://www.geeksforgeeks.org/types-of-literals-in-c-c-with-examples/
+
+// The values assigned to each constant variables are referred to as the literals
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    const int a = 10;
+    const float b = 2.3;
+    const char c = 'C';
+    const char s[20] = "VS Code";
+    const string f = "VS Code";
+    const bool g = true;
+}
