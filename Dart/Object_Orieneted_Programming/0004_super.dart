f10d0b488694104b3d7baa822b8b22bbe279bda3
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-08 22:52:15 +0600

    Update 0004_super.dart

--- Dart/Object_Orieneted_Programming/0004_super.dart
+++ Dart/Object_Orieneted_Programming/0004_super.dart
@@ -16,7 +16,13 @@ class Student extends Person {
   int id;
   
   // here we use "super" keyword to initialize the super class constructor by passing data from sub class constructor.
-  Student(this.name, this.age, this.id) : super(name, age);
+  // The superclass constructor must be called in SubExample because the compiler has to somehow initialize the variable a of Example.
+ 
+  Student(String name, int age, int id)  : _name = name, _age = age, 
+            _id = id,  super(name, age + 10);
+ 
+  // sorthand format, but in case we have found that we can't modify the passing data to super class from sub class.
+  // Student(this.name, this.age, this.id) : super(name, age);
 
   
   
