0ced5bd1750f3a0e3237bc693f8169897118b9ab
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-09 11:20:38 +0600

    Update 0004_super.dart

--- Dart/Object_Orieneted_Programming/0004_super.dart
+++ Dart/Object_Orieneted_Programming/0004_super.dart
@@ -1,7 +1,7 @@
 class Person {
   String name;
   int age;
-  
+
   Person(this.name, this.age);
   void display() {
     print('$name, $age');
@@ -9,34 +9,29 @@ class Person {
 }
 
 class Student extends Person {
-  @override
-  String name;
-  @override
-  int age;
-  int id;
-  
+  final String _name;
+  final int _age;
+  final int _id;
+
   // here we use "super" keyword to initialize the super class constructor by passing data from sub class constructor.
   // The superclass constructor must be called in SubExample because the compiler has to somehow initialize the variable a of Example.
- 
-  Student(String name, int age, int id)  : _name = name, _age = age, 
-            _id = id,  super(name, age + 10);
- 
-  // sorthand format, but in case we have found that we can't modify the passing data to super class from sub class.
-  // Student(this.name, this.age, this.id) : super(name, age);
 
-  
-  
+  Student(String name, int age, int id) : _name = name, _age = age, _id = id, super(name, age + 10);
+
+  // sorthand format, but in case we have found that we can't modify the passing data to super class from sub class
+  // if the sub class & super class member variables name are same.
+  // Student(this.name, this.age, this.id) : super(name, age);
+  // Student(this._name, this._age, this._id) : super(_name, _age + 10);
+
   @override
   void display() {
-    
     // "super" keywork is used to access the super class method from sub class.
-    
+
     super.display();
-    print('$id');
+    print('$_name, $_age');
+    print('$_id');
   }
 }
-
-
 
 void main() {
   Student s1 = Student('Sazin', 24, 11);
