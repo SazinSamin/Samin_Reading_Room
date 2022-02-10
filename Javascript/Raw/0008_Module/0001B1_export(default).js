68e1441d18e420db8adbfcfee55e58cc1d486d4f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-10 10:50:40 +0600

    Update and rename 0001B1_export(default).js to 0001C_export(default).js

--- Javascript/Raw/0008_Module/0001B1_export(default).js
+++
@@ -1,5 +0,0 @@
-// default import doesn't uses curly braces {}
-// we can change the name of any named function when use default import
-import multi from "./script.js";
-
-console.log(multi(10, 20));
---
+++ Javascript/Raw/0008_Module/0001C_export(default).js
@@ -0,0 +1,17 @@
+// default export is shorthand form
+// only works for function
+// only one defaule export could be in one module
+// default export could be named or anonymous function
+
+
+export default add(a, b) {
+        return a * b;
+}
+
+
+// we can use below syntax also
+
+/* export default add;
+export function add(a, b) {
+        return a * b;
+}*/
