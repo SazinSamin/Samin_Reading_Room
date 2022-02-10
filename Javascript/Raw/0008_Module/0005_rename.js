479c6d405136c84dbd9638f833abb378bbc963c4
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-10 10:55:44 +0600

    Create 0005_rename.js

---
+++ Javascript/Raw/0008_Module/0005_rename.js
@@ -0,0 +1,7 @@
+// we can rename in time of import. 
+import {addition as add, sub as subtraction, stu as student} from "./script.js";
+
+
+// we can also rename in time of export.
+export {add as addition, subtruct as sub, Student as stu};
+
