6742e0b09ef6cdf4099ba7245bf3899ee3074244
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-10 09:54:32 +0600

    Update 0003_import_*_(all).js

--- Javascript/Raw/0008_Module/0003_import_*_(all).js
+++ Javascript/Raw/0008_Module/0003_import_*_(all).js
@@ -3,5 +3,6 @@ import * as script from "./script.js";
 const s1 = new script.Student('Green', 24, 3.84);
 s1.display();
 
-console.log(script.add(10, 20));
+console.log(script.add(10, 20));        
 console.log(script.subtruct(20, 10));
+console.log(`value of PI: ${script.PI}`);
