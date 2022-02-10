1c173ff34a589e7bad7ac85353eb9f58f2b77b7c
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-10 10:51:10 +0600

    Create 0004_import(default).js

---
+++ Javascript/Raw/0008_Module/0004_import(default).js
@@ -0,0 +1,5 @@
+// default import doesn't uses curly braces {}
+// we can change the name of any named function when use default import
+import multi from "./script.js";
+
+console.log(multi(10, 20));
