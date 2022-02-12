21a090a18ab7ef11fe7c01caeeb1a8d0252fd474
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-12 11:10:53 +0600

    Create 0008_range_of_characters.js

---
+++ Javascript/RegEx/0008_range_of_characters.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/match-letters-of-the-alphabet
+
+
+const string1 = "Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many waterways. Its Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common. On the southern coast, the Sundarbans, an enormous mangrove forest shared with Eastern India, is home to the royal Bengal tiger";
+
+
+// using character classes we have to use individual characters to find out the result. but if the character are many then it will be tedious.
+// so can use hyphen"-" to select a range of characters.
+const regEx = /[a-s]/gi;
+const result = string1.match(regEx);
+console.log(result);
