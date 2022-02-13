be4b23b7026a347a2830b993e77a758dc2ab9d9d
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-13 09:17:17 +0600

    Create 0013_match_at_begining.js

---
+++ Javascript/RegEx/0013_match_at_begining.js
@@ -0,0 +1,12 @@
+// *** carefull, carat also used for negated character set
+// link: https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/RegEx/0009_negated%20character%20sets.js
+
+
+// caract symbol (^) used as to match a pattern at the beginning of a string.
+// if the match is not in the beginning of the string, the match will fail or test will return false. 
+
+
+const string1 = "United International Univesity";
+const regEx = /^United/gi;
+const result = string1.match(regEx);
+console.log(result);    
