6f35e3015650fb1bba7022882ba214624424cba5
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-15 22:38:35 +0600

    Update example1.js

--- Javascript/Data Structure/Array/example1.js
+++ Javascript/Data Structure/Array/example1.js
@@ -1,15 +1,14 @@
 // arrray in JS is mutable.
 
 
+
 // array elements could be anything.
 const array = [1, 2, 3, 4, "String", true];
 
 
+
 // access an element in array.
 console.log(array[0]);
-
-
-
 // change a element in array.
 array[1] = 100;
 
@@ -17,9 +16,15 @@ array[1] = 100;
 
 // add elements in last of the all elements in array.
 array.push(120);
-
-
 // add elements in first of the all elements in array.
 array.unshift(130);
 
+
+// remove elements in last of the all elements in array.
+// ** both popped & shift return the deleted element,
+const popped = array.pop();
+// remove elements in first of the all elements in array.
+const shifted = array.shift();
+
 console.log(array);
+
