a5eb3200415e120c82ffa34f973d839ca8293b03
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-15 15:10:13 +0600

    Create example1.js

---
+++ Javascript/Data Structure/Array/example1.js
@@ -0,0 +1,25 @@
+// arrray in JS is mutable.
+
+
+// array elements could be anything.
+const array = [1, 2, 3, 4, "String", true];
+
+
+// access an element in array.
+console.log(array[0]);
+
+
+
+// change a element in array.
+array[1] = 100;
+
+
+
+// add elements in last of the all elements in array.
+array.push(120);
+
+
+// add elements in first of the all elements in array.
+array.unshift(130);
+
+console.log(array);
