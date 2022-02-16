1efd28b382e6743cf7072b37c36b3350946b711b
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-16 08:46:29 +0600

    Create indexOf.js

---
+++ Javascript/Data Structure/indexOf.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+
+
+// indexOf take an element & return that element index.
+// if element not found in array it will return -1.
+// ** indexOf use strict equality.
+
+
+const array = [10, 20, 120, 140, 23];
+const idx = array.indexOf(120);
+console.log(idx);
