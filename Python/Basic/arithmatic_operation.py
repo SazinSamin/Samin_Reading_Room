93c0e46a9a3ed2d6e4ed7c623ba5a3b2a0f1ba13
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-17 07:30:35 +0600

    Create arithmatic_operation.py

---
+++ Python/Basic/arithmatic_operation.py
@@ -0,0 +1,23 @@
+a = 10
+b = 2.7
+
+# add a with b
+print(a + b)
+
+# subtract a with b
+print(a - b)
+
+# multiply a with b
+print(a * b)
+
+# divide a with b
+print(a / b)
+
+# raise a to the power of b. If b is an no-integer, it will become a root. 
+print(a ** b)
+
+# gives the integral part of a divided by b.
+print(a // b)
+
+# gives the remainder of a divided by b.
+print(a % b)
