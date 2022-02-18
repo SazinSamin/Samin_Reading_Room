53e09a321a9de4f0b283e28d5c514c3ba0df347f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-18 08:03:14 +0600

    Create reverse.js

---
+++ Javascript/Data Structure/reverse.js
@@ -0,0 +1,17 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+
+const array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array);
+
+
+// carefull, this below code doesn't work properly.
+const obj1 = {
+        1: 6,
+        2: 4,
+        3: 2,
+        length: 3
+};
+
+Array.prototype.reverse.call(obj1);
+console.log(obj1);
