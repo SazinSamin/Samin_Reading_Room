160bc345fc25a2f52039b4c069940af46f095776
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-18 14:44:00 +0600

    Update 0013_arrowFunction.js

--- Javascript/Raw/0013_arrowFunction.js
+++ Javascript/Raw/0013_arrowFunction.js
@@ -3,6 +3,15 @@
 let myFunction = (a, b) => a + b;
 console.log(myFunction(20, 20));
 
+// if we use the traditional curly brackets after the arrow function, then we have to use the "return"
+// keyword inside, otherwise it won't gonna return any value.
+let myFunction2 = (a, b) => {
+        return a + b;
+};
+console.log(myFunction2(20, 20));
+
+
+
 
 // in case of one parameter we can omit the braces.
-let myFunction = a => a;
+let myFunction3 = a => a;
