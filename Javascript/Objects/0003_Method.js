7d5d4f8a92ae8c1174179bc91890d61d087265cf
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-21 17:26:31 +0600

    Update 0003_Method.js

--- Javascript/Objects/0003_Method.js
+++ Javascript/Objects/0003_Method.js
@@ -4,17 +4,29 @@ const person1 = {
         firstName: "Sazin",
         lastName: "Samin",
         birthYear: 1999,
+        
         // object method
         details: function () {
                 return `Name: ${this.firstName} ${this.lastName}\n birthYear: ${this.birthYear}`;
+        },
+        
+        // we can refer to the object by avobe style {this.property} or by refering
+        // own variable by {objectName.property}. But the above one (this.property) is perfect & reusable,
+        // because the {objectName.property} has a serious drawback that if the objectName has
+        // changed, we have changed all the reference or objectName as well.
+        
+        getFirstName: function() {
+                return "FirstName: " + person1.firstName;
         }
 }
 
 console.log(person1.details());
+console.log(person1.getFirstName());
 // without "()", the funciton defintion will return
 
+
 // addition of a method
-person1.age = function(currentYear){
+person1.age = function (currentYear) {
         return currentYear - this.birthYear;
 }
 
