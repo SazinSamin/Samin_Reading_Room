d9af5ebf8d75b45a083bc6548a1f520a46fd4847
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-26 09:09:43 +0600

    Update map().js

--- Javascript/Data Structure/map().js
+++ Javascript/Data Structure/map().js
@@ -1,1 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
+
+// map do the given operation on each element & return  a new array by adding the result to the new array
+
+const array = [1, 2, 3, 4, 5];
+const double = array.map((ele) => ele * 2);
+
+console.log(double);
