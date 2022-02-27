efbe4632ee197e3cdccb7566dd9eafc97ade46e3
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-27 18:16:51 +0600

    Update generateIP.dart

--- Dart/Random/Ping_to_all_IP/generateIP.dart
+++ Dart/Random/Ping_to_all_IP/generateIP.dart
@@ -1,27 +1,22 @@
-int i = 1, j = 1, k = 0, l = 0;
-List<int> ip_add = [i, j, k, l];
+int i = 0, j = 0, k = 0, l = 0;
 int MAX = 255;
 
 String getIP() {
 
   if(l < MAX) {
     l++;
-    return '$i.$j.$k.$l';
   } else if (k < MAX) {
     k++;
     l = 0;
-    return '$i.$j.$k.$l';
   } else if (j < MAX) {
     j++;
     k = 0;
     l = 0;
-    return '$i.$j.$k.$l';
   } else if (l < MAX) {
     i++;
     j = k = l = 0;
-    return '$i.$j.$k.$l';
   }
-  return '';
+  return '$i.$j.$k.$l';;
 }
 
 void main() {
