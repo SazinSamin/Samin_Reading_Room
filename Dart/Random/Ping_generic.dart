28203a3a11c71351cc56a48dc0f499eea9635c79
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-28 10:25:05 +0600

    Update Ping_generic.dart

--- Dart/Random/Ping_generic.dart
+++ Dart/Random/Ping_generic.dart
@@ -1,13 +1,21 @@
+// know about process
+// https://api.dart.dev/stable/2.16.1/dart-io/Process-class.html
+// https://stackoverflow.com/questions/67338711/dart-process-run-get-stdout-as-stream-instead-of-string-just-like-process
+
 import 'dart:convert';
 import 'dart:io';
 
 
 void unixCmd() async{
-  var result = await Process.start('ping', ['-c 5','192.168.0.1'] );
+  // var result = await Process.start('traceroute', ['google.com'] );
+  var result = await Process.start('ping', ['-c 5','google.com']);
   print(result.toString());
   await result.stdout.transform(utf8.decoder).forEach((element) {
     print(element);
   });
+
+  print(await result.exitCode);
+
 }
 
 
