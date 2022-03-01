b9fb141a3de5c367c6c42da3b1a259e485c5833a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-01 14:42:05 +0600

    Update 0003_Prototype_example.js

--- Javascript/OOP/Prototype Based/0003_Prototype_example.js
+++ Javascript/OOP/Prototype Based/0003_Prototype_example.js
@@ -4,9 +4,11 @@ const s = [1, 2, 3, 4, 5, 6, 7, 8];
 // herer we also add another method to the Javascript Array Object Prototype.
 // so we can use that method on a array like other built in method(slice, splice, map, reduce)
 
-Array.prototype.myMap = function(callback) {
+
+// implementation of own "map" method.
+Array.prototype.myMap = function (callback) {
     const newArr = [];
-    for(let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         newArr.push(callback(this[i]));
     }
     return newArr;
@@ -14,3 +16,24 @@ Array.prototype.myMap = function(callback) {
 
 const res = s.myMap((ele) => ele * 2);
 console.log(res);
+
+
+
+
+
+// implementation of own "filter()" method
+Array.prototype.myFilter = function (callback) {
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i])) {
+            newArray.push(this[i]);
+        }
+    }
+    return newArray;
+};
+
+const res1 = s.myFilter(function (item) {
+    return item % 2 === 1;
+});
+
+console.log(res1);
