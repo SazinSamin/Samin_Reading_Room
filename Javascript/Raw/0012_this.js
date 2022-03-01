4ff9651aabffe458cfbf7889381aec8731a4b2b4
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-01 12:19:08 +0600

    Update 0012_this.js

--- Javascript/Raw/0012_this.js
+++ Javascript/Raw/0012_this.js
@@ -1,4 +1,6 @@
 // https://www.w3schools.com/js/js_this.asp
+
+
 // "this" refer to that object from where it calling.
 
 const Person = {
@@ -18,3 +20,22 @@ console.log(Person.myFunction().age);
 // in outside any method this refer to the global object
 console.log(this);
 
+
+
+
+// here "this" will replace by the element on which the the function has called.
+const s = [1, 2, 3, 4, 5, 6, 7, 8];
+
+Array.prototype.myMap = function(callback) {
+        
+    const newArr = [];
+    
+    for(let i = 0; i < this.length; i++) {
+        newArr.push(callback(this[i]));
+    }
+        
+    return newArr;
+}
+
+const res = s.myMap((ele) => ele * 2);
+console.log(res);
