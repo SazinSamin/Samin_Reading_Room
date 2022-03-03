5fda6372ff44f7f94e5dc4dfd868739e59680875
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-03 11:08:01 +0600

    Create some().js

---
+++ Javascript/Data Structure/some().js
@@ -0,0 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+
+const arr = [10, 20, 30, 40, 50, 60];
+
+// some() will return true if one of the element statisfy the condition.
+// otherwise return false.
+// it doesn't mutate the original array.
+// call on an empty function return false in any condition.
+const res =  arr.some((ele) => ele > 20);
+console.log(res);
