72423d064f310976c9d57cda7ae006e19d668f7f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-06 22:05:05 +0600

    Create numbers.js

---
+++ Javascript/Raw/Numbers/numbers.js
@@ -0,0 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MIN_SAFE_INTEGER
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+
+
+const min = Number.MIN_SAFE_INTEGER;
+const max = Number.MAX_SAFE_INTEGER;
+
+console.log(min);
+console.log(max);
