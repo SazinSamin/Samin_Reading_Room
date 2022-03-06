f76863010013a67abe1dd7326225d5f3d8319112
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-06 22:29:48 +0600

    Create apply().js

---
+++ Javascript/Methods/apply().js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+
+const arr = [2, 5, 8, 7, 9, 3];
+console.log(Math.min.apply(null, arr));
+
+
+
+// slight problem in below code.
+function getMax(arg) {
+    const len = arg.length;
+    let min = +Infinity;
+    for(let i = 0; i < len; i++) {
+        if(arg[i] < min) {
+            min = arg[i];
+        }
+    }
+    return min;
+}
+
+// arr should pass alone, but to get the correct result we need send between square bracket.
+const res = getMax.apply(null, [arr]);
+console.log(res);
