a709191ead1eb880010b3dfdea375d24fcea79ad
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-08 21:46:33 +0600

    Create include().js

---
+++ Javascript/Data Structure/include().js
@@ -0,0 +1,8 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+const arr = [10, 20, 30, 40, 50, 60];
+
+// include() return true if a certain value present in the array, otherwise return false.
+// the method is case sensitive.
+const res =  arr.includes(20);
+console.log(res);
