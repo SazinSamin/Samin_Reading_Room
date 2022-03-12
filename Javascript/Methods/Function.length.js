f0eb6b362d3b07dc2e04a986f602940a2b3db3f9
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-12 22:56:39 +0600

    Create Function.length.js

---
+++ Javascript/Methods/Function.length.js
@@ -0,0 +1,13 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/length
+
+function x(a, b) {}
+function y(a, b, c) {}
+function z() {}
+function f(...rest) {}
+function g(a, b = 1, c) {}
+
+console.log(x.length); // 2
+console.log(y.length); // 3
+console.log(z.length); // 0
+console.log(f.length); // 0, rest paramter is not counter.
+console.log(g.length); // only the parameter before the first default value paramter is counted.
