ee43eec71faf2a746a6077d0d7a8dfecb97ba4a0
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-13 09:17:36 +0600

    Create type_conversion.py

---
+++ Python/Basic/type_conversion.py
@@ -0,0 +1,22 @@
+# https://www.programiz.com/python-programming/type-conversion-and-casting#:~:text=Type%20Conversion%20is%20the%20conversion,data%20in%20Implicit%20Type%20Conversion.
+
+
+a = 10
+b = 20.20
+
+# implicit data type convertion,
+# here the interger type 10 convert to float, then a & b add together.
+# implicit conversion done by python interpreter when there is no chance of data loss.
+
+c = a + b
+print(c)
+
+
+x = "12";
+
+# explicit conversion
+# it is also called type casting.
+
+q = int(x);
+r = str(b);
+s = float(a);
