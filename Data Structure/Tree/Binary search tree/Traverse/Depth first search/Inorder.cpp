9745b6226df1f96fdf0ac2a305bb68972d9c9d7c
samin <sazinsamin@gmail.com>
2022-03-17 09:54:16 +0600

    Changes in files & folders.

--- Data Structure/Tree/Binary search tree/Traverse/Depth first search/Inorder.cpp
+++
@@ -1,9 +0,0 @@
-void traverse(struct Node* head){
-        if(head == NULL){
-                return;
-        }
-
-        traverse(head->left);
-        cout<<head->data<<" ";
-        traverse(head->right);
-}
