bd478a0e9cbf978482e72c3bd130b48276c9a1f3
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-03-24 09:51:00 +0600

    Update data_type.py

--- Python/Basic/data_type.py
+++ Python/Basic/data_type.py
@@ -8,3 +8,12 @@ c = "Ahisque"
 print(type(a));
 print(type(b));
 print(type(c));
+
+
+def get(a) :
+        print(a)
+
+# None
+# None indicate that things are empty or return noting.
+res = get(a);
+print(res);
