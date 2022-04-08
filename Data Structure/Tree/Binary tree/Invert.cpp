966f5a5f6822cf3465c4d7fba18f82a457c7b022
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-04-08 23:33:32 +0600

    Update Invert.cpp

--- Data Structure/Tree/Binary tree/Invert.cpp
+++ Data Structure/Tree/Binary tree/Invert.cpp
@@ -1,15 +1,17 @@
+// https://www.educative.io/edpresso/how-to-invert-a-binary-tree
+// https://leetcode.com/problems/invert-binary-tree/
+
+
 void invert(struct Node* head){
-        if(head == NULL){
-                return;
-        }else{
-                struct Node* temp;
-
-                invert(head->left);
-                invert(head->right);
-
-                temp = head->left;
-                head->left = head->right;
-                head->right = temp;
-        }
+    if(!root) return root;
+        
+    invert(root->left);
+    invert(root->right);
+        
+    Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+        
+    return root;
 }
 
