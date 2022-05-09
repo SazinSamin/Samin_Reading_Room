7e5044e93c6ffeed15d39a5f1c1a67c7e67bd0a8
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-05-09 22:03:15 +0600

    Update 0006_for_loop.py

--- Python/Basic/0006_for_loop.py
+++ Python/Basic/0006_for_loop.py
@@ -1,7 +1,17 @@
+# if we give 1 parameter to the range function it will iterate to that parameter, incrementing by 1.
+# if we give 2 paramter it will start from that parameter, to another parameter, incrementing by 1.
+# if we give 3 paramter it will incrementing the value by that paramter.
+
+# in default range funciton start from 0, end just before the number of second argument.
+
+
 for i in range(2, 20):
 	print(i)
 
 for i in range(10):
+	print(i)
+	
+for i in range(5, 100, 10):
 	print(i)
 
 
