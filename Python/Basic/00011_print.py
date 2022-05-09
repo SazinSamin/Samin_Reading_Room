acafa869e98db2958231dcee58cf52106c772fb3
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-05-09 22:03:36 +0600

    Create 00011_print.py

---
+++ Python/Basic/00011_print.py
@@ -0,0 +1,9 @@
+# https://www.programiz.com/python-programming/methods/built-in/print
+
+a = 'Md.'
+b = 'Tofajjel'
+c = 'Hossain'
+
+# sep for sperater between the print contens.
+# end for how the line ends with
+print(a, b, c, sep = '_', end = '##')
