07865c6c2694501f68e4045ffbf9286ffc9eacce
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-05-13 09:48:16 +0600

    Update 0005_while_loop.py

--- Python/Basic/0005_while_loop.py
+++ Python/Basic/0005_while_loop.py
@@ -2,3 +2,8 @@ i = 0
 while i < 10:
 	print(i)
 	i += 1
+	
+	
+# Common pitfall: from coursera
+# Failure to initialize variables. Make sure all the variables used in the loop’s condition  are initialized before the loop.
+# Unintended infinite loops. Make sure that the body of the loop modifies the variables used in the condition, so that the loop will eventually end for all possible values of the variables.
