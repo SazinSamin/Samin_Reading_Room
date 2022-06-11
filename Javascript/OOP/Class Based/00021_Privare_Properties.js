48cc73ec05201a312e48d57cca3dafd43eae7658
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-06-11 23:17:02 +0600

    Update 00021_Privare_Properties.js

--- Javascript/OOP/Class Based/00021_Privare_Properties.js
+++ Javascript/OOP/Class Based/00021_Privare_Properties.js
@@ -1,30 +1,33 @@
+
+
+
 // private variables & methods have only scope in class, means they are only avaiable inside the class,
 // to declear a private property start the property name with #. 
 
 class Person {
-        firstName;
-        lastName;
-        // private variable, decleare with #, 
-        #age;
+    firstName;
+    lastName;
+    // private variable, decleare with #, 
+    #age;
 
-        constructor(fName, lName, age) {
-                this.firstName = fName;
-                this.lastName = lName;
-                this.#age = age;
-        };
+    constructor(fName, lName, age) {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.#age = age;
+    };
 
-        getName() {
-                return `Name: ${this.firstName} ${this.lastName}, age: ${this.#getAge()}`;
-        }
+    getName() {
+            return `Name: ${this.firstName} ${this.lastName}, age: ${this.#getAge()}`;
+    }
 
-        // private method, declear with #.
-        #getAge() {
-                return this.#age;
-        }
+    // private method, declear with #.
+    #getAge() {
+            return this.#age;
+    }
 }
 
 const p1 = new Person("Ahsique", "Ahmed", 25);
 console.log(p1.firstName);
-console.log(p1.age);    // leads to syntex error, we can't access private variable outside the class.
+console.log(p1.age);    // undefined
 console.log(p1.getName());
-console.log(p1.getAge());    // leads to syntex error, we can't access private method outside the class.
+console.log(p1.getAge()); // TypeError: p1.getAge is not a function
