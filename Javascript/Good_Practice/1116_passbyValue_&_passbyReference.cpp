284e88995b38cf6e8eca3b4520f1c4d30490acb1
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-06-13 11:55:03 +0600

    Update 1116_passbyValue_&_passbyReference.cpp

--- Javascript/Good_Practice/1116_passbyValue_&_passbyReference.cpp
+++ Javascript/Good_Practice/1116_passbyValue_&_passbyReference.cpp
@@ -1,6 +1,8 @@
 // https://www.geeksforgeeks.org/pass-by-value-and-pass-by-reference-in-javascript/
 // https://codeburst.io/javascript-passing-by-value-vs-reference-explained-in-plain-english-8d00fd06a47c
 
+// deep & shalow copy -> https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Good_Practice/1115_deep_%26_shallow_copy.js
+
 
 // primitive value like number, string, undefined are passed by value in Js.
 // function passed as parameter also passed by value.
@@ -22,4 +24,21 @@ console.log(test(arr));
 console.log(arr);
 
 
+
+// changes to any object will reflect to other object as well.
+
+const person1 = {
+    name: 'Ahmad',
+    age: 24,
+    location: 's korea'
+};
+
+console.log(person1);
+
+const person2 = person1;
+person2.location = 'n korea';
+
+
+// after location change
+console.log(person1);
 
