87afd9a6ae01306eb76166519950dd4d3820cd1a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-06-14 10:00:10 +0600

    Update 0007_Prototype.js

--- Javascript/Objects/0007_Prototype.js
+++ Javascript/Objects/0007_Prototype.js
@@ -1,6 +1,15 @@
 // first know what is prototype in below link:
 // https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Objects/0002_OwnProperty_%26_Prototype.js
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
+// https://www.youtube.com/watch?v=6WBaPpFJhR0&list=PLHiZ4m8vCp9OkrURufHpGUUTBjJhO9Ghy&index=70
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes.
+
+
+
+// JS is a prototype based language
+// prototype helps us to add property directly outside the constructor function
+// every object in js has prototype & prototype chain.
+// prototype also use for inhertance because if we add a prototype to a object, it will be avaiable to all instance.
+// we can also add prototype to js built-in object like String, Number, Boolean (which is highly un-recommended)
 
 
 function Person(first, last, age) {
