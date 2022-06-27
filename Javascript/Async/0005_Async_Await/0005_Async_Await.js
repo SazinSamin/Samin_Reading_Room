5e683c670f04829ee82d93409d08c827221fd05f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-06-27 23:02:52 +0600

    Update 0005_Async_Await.js

--- Javascript/Async/0005_Async_Await/0005_Async_Await.js
+++ Javascript/Async/0005_Async_Await/0005_Async_Await.js
@@ -1,17 +1,14 @@
 import fetch from "node-fetch";
 
-const weatherData = async () => {
+const weather = async () => {
     return await (await fetch("http://api.openweathermap.org/data/2.5/weather?q=Jhenaidah&appid=263241df63fdd1b6dd713b14834fb2eb"))
                            .json();
 }
 
 try {
-    console.log(await weatherData());
+    console.log(await weather());
 } catch(e) {
-    console.trace(e);
+    console.log(e);
 }
-
-
-
 
 
