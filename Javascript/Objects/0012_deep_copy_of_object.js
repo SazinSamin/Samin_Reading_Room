b7878fc640c57aa65ae181b9d1a6c779d9591c5c
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-06-28 20:30:56 +0600

    Update 0012_deep_copy_of_object.js

--- Javascript/Objects/0012_deep_copy_of_object.js
+++ Javascript/Objects/0012_deep_copy_of_object.js
@@ -4,7 +4,9 @@
 // https://developer.mozilla.org/en-US/docs/Glossary/Deep_copy
 // https://code.tutsplus.com/articles/the-best-way-to-deep-copy-an-object-in-javascript--cms-39655
 
-// 
+
+
+// object construction
 const obj1 = {
     name: 'ahmed',
     age: 35,
@@ -14,8 +16,11 @@ const obj1 = {
     },
 };
 
+
+// convert object to string
 const obj1String = JSON.stringify(obj1);
 
+// return to object from the string
 const obj2 = JSON.parse(obj1String);
 
 typeof(obj2) === 'object' ? console.log(obj2) : '';
