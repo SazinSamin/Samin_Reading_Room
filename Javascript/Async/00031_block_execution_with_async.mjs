02a3f2be5b6f452f343e93d389ad79a3cac6b29a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-08-20 19:30:02 +0600

    Create 00031_block_execution_with_async.mjs

---
+++ Javascript/Async/00031_block_execution_with_async.mjs
@@ -0,0 +1,19 @@
+import fetch from "node-fetch";
+
+
+// The asynchronous operation means the result will  come later.
+// if we want to block the other operation until the asynchronous operation result comes
+// then we can define a function with async, await and call the function.
+// when we call the funciton it will be synchronous as normal function is synchronous
+// so until the result of async doesn't come, it won't go to the next statement.
+
+const weather = async () => {
+        return await (await fetch('https://virusbase.herokuapp.com'))
+                .json();
+}
+
+console.log(await weather());
+for(let i = 0; i <= 50000; i++) {
+        console.log(i);
+}
+console.log('Finished');
