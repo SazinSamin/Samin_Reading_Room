ccc565f50291eebd5a7233f2e80de17bcac8619a
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-08-29 11:23:24 +0600

    Create lastIndexOf.js

---
+++ Javascript/Data Structure/lastIndexOf.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+
+const arr = [2, 4, 3, 4, 2, 4, 6, 7, 1, 2];
+console.log(arr.lastIndexOf(2));
