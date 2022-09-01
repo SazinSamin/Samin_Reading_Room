6409571e3046f4113c7b3dcae634a9f0bfa05ef7
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-01 14:46:49 +0600

    Create linked_list_javascript.js

---
+++ Data Structure/Linked List/linked_list_javascript.js
@@ -0,0 +1,31 @@
+class ListNode {
+        constructor(val, next) {
+                this.val = (val === undefined ? 0 : val);
+                this.next = (next === undefined ? null : next);
+        }
+}
+
+const insert = (head, val) => {
+        const node = new ListNode(val);
+        if(head == null) {
+                return node;
+        }
+        node.next = head;
+        head = node;
+        return head;
+}
+
+const printNodeVal = (head) => {
+        while(head != null) {
+                console.log(head.val);
+                head = head.next;
+        }
+}
+
+let head = null;
+const arr = [5, 4, 3, 2, 1];
+for(let i = 0; i < arr.length; i++) {
+        head = insert(head, arr[i]);
+}
+
+printNodeVal(head);
