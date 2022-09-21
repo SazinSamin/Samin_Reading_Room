9fd741df873caf845594a948ac2d59095f2db8ab
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-21 17:50:10 +0600

    Update 113_arrowFunc.js

--- Javascript/Raw/0007_Function/113_arrowFunc.js
+++ Javascript/Raw/0007_Function/113_arrowFunc.js
@@ -1,5 +1,5 @@
 // arrow function
-// "this" doesn't work inside arrow function.
+// "this" behave different in arrow function. => https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Good_Practice/1117_this_in_arrow_function.js
 
 const myFunction = (a, b) => { return a + b; };
 console.log(myFunction(10, 20));
