763c4d770bfef6fd0a8613a89465cc7011ca772e
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-21 17:39:16 +0600

    Update 1117_this_in_arrow_function.js

--- Javascript/Good_Practice/1117_this_in_arrow_function.js
+++ Javascript/Good_Practice/1117_this_in_arrow_function.js
@@ -1,4 +1,8 @@
 // "this" behave different in javascript arrow function.
 
+// in regular function "this" represent the object that called the function.
+// in arrow function "this" represent the owner's of the function.
+
+
 // https://www.youtube.com/watch?v=5ccXpH0nlc8
 // https://www.w3schools.com/js/js_arrow_function.asp
