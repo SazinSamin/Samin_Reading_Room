1eb983153681ea8d78c38bd9f2c0000d104babe4
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-30 12:18:23 +0600

    Update 1115_const.js

--- Javascript/Raw/3333_Scope/1115_const.js
+++ Javascript/Raw/3333_Scope/1115_const.js
@@ -1,5 +1,11 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
 
+// declear variable with "const", we have to assign the value immediatly there.
+// also we can't reassign the value again.
+
+
+
+
 const x = 10;
 
 try{
@@ -9,3 +15,7 @@ try{
 }
 
 console.log(x);
+
+// we can't do this with "const"
+// const y;
+// y = 12;
