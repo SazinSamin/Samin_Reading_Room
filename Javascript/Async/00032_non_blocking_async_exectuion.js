df4a03fbcd5ce01aa1c258063bfab87db0a315c8
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-07 09:23:27 +0600

    Update 00032_non_blocking_async_exectuion.js

--- Javascript/Async/00032_non_blocking_async_exectuion.js
+++ Javascript/Async/00032_non_blocking_async_exectuion.js
@@ -1,11 +1,5 @@
 import fetch from "node-fetch";
 
-
-const weather = async () => {
-        return  (await fetch('https://virusbase.herokuapp.com')).json();
-}
-
-
 const weather = async () => {
         const x =  await fetch('https://virusbase.herokuapp.com');
         const y = await x.json();
@@ -13,6 +7,15 @@ const weather = async () => {
 }
 
 weather();
+
+for (let i = 0; i <= 50; i++) {
+        console.log(i);
+}
+console.log('Finished');
+
+
+
+
 
 // with then example
 /*
@@ -22,9 +25,3 @@ weather().then(msg => {
         console.log(e);
 })
 */
-
-
-for (let i = 0; i <= 50; i++) {
-        console.log(i);
-}
-console.log('Finished');
