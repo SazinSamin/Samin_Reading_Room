40ddb5e1770bd0c5f3990cf272d02c4015ed2be6
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-07 22:09:51 +0600

    Update 006_passbyValue_&_passbyReference.js

--- Javascript/Raw/3334_Primitive_vs_Reference/006_passbyValue_&_passbyReference.js
+++ Javascript/Raw/3334_Primitive_vs_Reference/006_passbyValue_&_passbyReference.js
@@ -4,6 +4,19 @@
 // deep & shalow copy -> https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/Good_Practice/1115_deep_%26_shallow_copy.js
 
 
+
+
+// in below line I have the previous concept, but I found this to pass a parameter to the function is pass by value or pass
+// by reference is confusing.
+// there also have argument in stackoverflow.
+// learn with sumit channel concept was also different.
+// https://stackoverflow.com/a/32793507/10928027
+// https://www.youtube.com/watch?v=vK8fnE_-NI0&list=PLHiZ4m8vCp9Nflbo9a0pZuLscG_Xc7DKq&index=14
+
+
+
+
+
 // primitive value like number, string, undefined are passed by value in Js.
 // function passed as parameter also passed by value.
 // Object & Array passed by reference in Js.
