900d73ca8b887bc43f6c258fe8913b1a9fbf80e9
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-08 09:53:52 +0600

    Update 001_lexical_scoping.js

--- Javascript/Raw/3333_Scope/001_lexical_scoping.js
+++ Javascript/Raw/3333_Scope/001_lexical_scoping.js
@@ -1,4 +1,1 @@
-// https://stackoverflow.com/a/53062093/10928027
-// https://www.freecodecamp.org/news/javascript-lexical-scope-tutorial/
-
-// lexical scoping means if we have nested function, then inner function can access it's parent function properties.
+// https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Javascript/StackOverflow/2_lexicalScope.js
