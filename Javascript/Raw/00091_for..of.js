37af7cd120ba43edc22d47d47c09fbcbc3b6f775
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-09 08:49:23 +0600

    Update 00091_for..of.js

--- Javascript/Raw/00091_for..of.js
+++ Javascript/Raw/00091_for..of.js
@@ -1,6 +1,5 @@
 var list = ["red", "green", "yellow", "blue", "orange"];
-var myname = "Sazin Reshed Samin";
 
-for(var x of myname){
-        console.log(x);
+for (var ele of list) {
+        console.log(ele);
 }
