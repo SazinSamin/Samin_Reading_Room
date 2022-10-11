16c68995e1745eb1bb226698e1f60da70efecfea
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-11 12:15:58 +0600

    Update Javascript_execution_contest.js

--- Javascript/Javascript_execution_contest.js
+++ Javascript/Javascript_execution_contest.js
@@ -1,3 +1,7 @@
+// https://www.freecodecamp.org/news/execution-context-how-javascript-works-behind-the-scenes/#:~:text=The%20Execution%20Context%20contains%20the,and%20the%20code%20gets%20executed.
+// https://www.youtube.com/watch?v=Wk0-6b1W_VQ&list=PLHiZ4m8vCp9Nflbo9a0pZuLscG_Xc7DKq&index=10
+
+
 Gloabl execution context
 window: global object
 this: window // this points to window object 
