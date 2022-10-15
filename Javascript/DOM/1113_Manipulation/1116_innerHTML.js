320e18a32ae37aa33565c8a41c457bce6f71327c
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-15 21:03:30 +0600

    Update and rename 1116_innerHTML.js to 1116_innerHTML_innterText_textContent.js

--- Javascript/DOM/1113_Manipulation/1116_innerHTML.js
+++
@@ -1,3 +0,0 @@
-// this take id and change the content of the id on the fly.
-
-document.getElementById("h1_1").innerHTML = "TypeScript"
---
+++ Javascript/DOM/1113_Manipulation/1116_innerHTML_innterText_textContent.js
@@ -0,0 +1,11 @@
+let ele1 = document.getElementById("id");
+
+// returns the text has written in html file
+console.log(ele1.textContent);
+
+// return the content that show in the webpage, like if we do uppercase in css, it will returned the text
+// with that change
+
+console.log(ele1.innerText);
+// return html in string format
+console.log(ele1.innerHTML);
