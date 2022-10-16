29f84546de8216d760525dd45ea805f27505fa18
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-16 11:59:17 +0600

    Update 1114_querySelector.js

--- Javascript/DOM/1112_Elements/1114_querySelector.js
+++ Javascript/DOM/1112_Elements/1114_querySelector.js
@@ -2,6 +2,13 @@
 
 // One thing have to remember always about QuerySelector that
 // it always use the CSS syntax.
+// like for class it use first dot(.) and for id have to give #(hash) at first like css.
 
-var getElement = document.querySelector('#article_1 .info1');
-getElement.style.color = "green";
+var getElement1 = document.querySelector('#article_1');
+getElement1.style.color = "green";
+
+var getElement2 = document.querySelector('.fruits');
+
+// multiple selection at once
+var getElement3 = document.querySelector('#article_1 .info1');
+
