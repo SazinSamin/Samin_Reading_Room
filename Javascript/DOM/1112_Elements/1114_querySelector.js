4be551dfa5ab9d629f864c27224209a774cb04d7
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-16 12:10:13 +0600

    Update 1114_querySelector.js

--- Javascript/DOM/1112_Elements/1114_querySelector.js
+++ Javascript/DOM/1112_Elements/1114_querySelector.js
@@ -1,4 +1,8 @@
 // return the first element match with the search
+// if there is mulitple item in one condition, like same name multiple class
+// then it return the first match
+// Also like there we have put to select last item of any item list, so if there are multiple items as last items, then it return
+// the first siblings
 
 // One thing have to remember always about QuerySelector that
 // it always use the CSS syntax.
