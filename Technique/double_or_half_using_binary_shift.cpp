49cc23c48a476dd2d9793cb4d2fccbfb98e7ba8f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-12-09 10:17:01 +0600

    Create double_or_half_using_binary_shift.cpp

---
+++ Technique/double_or_half_using_binary_shift.cpp
@@ -0,0 +1,15 @@
+// Qoura -> https://qr.ae/pr1HpS
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 100;
+    // doubling the number
+    int y = 100 << 1;
+    // half the number
+    int z = 100 >> 1;
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+}
