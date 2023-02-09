2b156027ca66463e6df95b4801b77fc51a673baf
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-09 12:30:09 +0600

    Create 0005_Select.sql

---
+++ Database/Structure Query Language/0005_Select.sql
@@ -0,0 +1,8 @@
+-- Select column from table
+SELECT Name FROM Student;
+
+-- select multiple column from table
+SELECT Name, Age FROM Student;
+
+-- select all column from table
+SELECT * FROM Student;
