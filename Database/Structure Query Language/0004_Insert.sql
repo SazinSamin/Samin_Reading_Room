aedbc1b51f6021a69927794a826f4964248d4483
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-09 11:41:42 +0600

    Create 0004_Insert.sql

---
+++ Database/Structure Query Language/0004_Insert.sql
@@ -0,0 +1,12 @@
+-- insert data with column name
+INSERT INTO Student (Roll, Name, Gender, Age) VALUES (11, "Sazin", "Male", 25);
+
+-- insert data without column name
+INSERT INTO Student VALUES (01, "Abid", "Male", 24);
+
+-- multiple insert
+INSERT INTO Student 
+VALUES 
+(17, "Akash", "Male", 25), 
+(18, 'Nahid', 'Male', 23);
+
