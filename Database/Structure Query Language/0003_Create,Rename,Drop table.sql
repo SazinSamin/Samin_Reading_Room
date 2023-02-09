bd4728c6783e51fd9d2a82d17402039918b0becf
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-09 10:28:06 +0600

    Update 0003_Create,Rename,Drop table.sql

--- Database/Structure Query Language/0003_Create,Rename,Drop table.sql
+++ Database/Structure Query Language/0003_Create,Rename,Drop table.sql
@@ -1,3 +1,4 @@
+-- Create a table
 CREATE TABLE student 
 (
   Roll int,
@@ -10,3 +11,12 @@ CREATE TABLE student
 
 -- if error comes like [Error: near "Roll": syntax error] then check the selected primary key name are inside
 -- parenthesis () or not.
+
+
+
+-- Rename a table(Won't work)
+RENAME TABLE Student TO Students;
+
+
+-- Drop a table
+DROP TABLE Student;
