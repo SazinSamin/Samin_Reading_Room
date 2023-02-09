c1958dbd76e38a31374a879013230614979f9a0f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-09 14:39:31 +0600

    Create 0007_Order_By.sql

---
+++ Database/Structure Query Language/0007_Order_By.sql
@@ -0,0 +1,5 @@
+-- sort the data by ascending order(mention ASC is not nessary, as it is default behaviour)
+SELECT * FROM countries ORDER BY area ASC;
+
+-- sort the data by descending order
+SELECT * FROM countries ORDER BY area DESC;
