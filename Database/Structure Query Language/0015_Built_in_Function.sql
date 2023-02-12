2dc1c123b6ee12d25073a7532e36c3a335262166
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-12 11:37:48 +0600

    Create 0015_Built_in_Function.sql

---
+++ Database/Structure Query Language/0015_Built_in_Function.sql
@@ -0,0 +1,15 @@
+-- String concatenation
+SELECT CONCAT('Poltu', 'Ahmed');
+
+SELECT CONCAT(Name, ' is ', Age, ' years old') AS Introduction FROM Student;
+
+
+SELECT GREATEST(1, 2, 5, 3, 4);
+SELECT LEAST(1, 2, 5, 3, 4);
+
+SELECT POW(2, 16);
+SELECT LOG(2);
+SELECT LOG10(2);
+SELECT TRUNCATE(12.234988232, 3);
+SELECT RAND();
+SELECT EXP(5);
