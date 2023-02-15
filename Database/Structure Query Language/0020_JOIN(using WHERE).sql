0b37b11fa163c0638aab95cc34b07d0d7e955de9
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-15 22:04:17 +0600

    Update 0020_JOIN(using WHERE).sql

--- Database/Structure Query Language/0020_JOIN(using WHERE).sql
+++ Database/Structure Query Language/0020_JOIN(using WHERE).sql
@@ -1,16 +1,16 @@
 SELECT * FROM countries, regions WHERE countries.region_id = regions.region_id;
 
-
+-- JOIN three tables
 SELECT countries.name, languages.language FROM countries, country_languages, languages 
 WHERE languages.language_id = country_languages.language_id AND country_languages.country_id = countries.country_id;
 
-
+-- JOIN three tables for Bangladesh
 SELECT countries.name, languages.language FROM countries, country_languages, languages 
 WHERE languages.language_id = country_languages.language_id 
 AND country_languages.country_id = countries.country_id 
 AND countries.name = "Bangladesh";
 
-
+-- count the number of language in Bangladesh by joining the table
 SELECT COUNT(languages.language) FROM countries, country_languages, languages 
 WHERE languages.language_id = country_languages.language_id 
 AND country_languages.country_id = countries.country_id 
