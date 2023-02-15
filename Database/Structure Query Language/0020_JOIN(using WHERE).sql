d584a05866e21ce976c629f7ce5b911be0c0f3a9
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-15 17:38:47 +0600

    Update and rename 0020_JOIN.sql to 0020_JOIN(using WHERE).sql

--- Database/Structure Query Language/0020_JOIN(using WHERE).sql
+++ Database/Structure Query Language/0020_JOIN(using WHERE).sql
@@ -9,3 +9,9 @@ SELECT countries.name, languages.language FROM countries, country_languages, lan
 WHERE languages.language_id = country_languages.language_id 
 AND country_languages.country_id = countries.country_id 
 AND countries.name = "Bangladesh";
+
+
+SELECT COUNT(languages.language) FROM countries, country_languages, languages 
+WHERE languages.language_id = country_languages.language_id 
+AND country_languages.country_id = countries.country_id 
+AND countries.name = "Bangladesh";
