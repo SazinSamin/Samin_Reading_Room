d88ac94c17d35645d109722a6fd0ed2a89a64466
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-16 21:17:45 +0600

    Update COALESCE.sql

--- Database/Structure Query Language/Advanced/COALESCE.sql
+++ Database/Structure Query Language/Advanced/COALESCE.sql
@@ -3,3 +3,8 @@
 -- Return the first NON-NULL value
 SELECT COALESCE(NULL, 5, 2, 'Muhib');  -- Output: 5
 
+
+-- from leetcode problem
+# Write your MySQL query statement below
+SELECT name FROM Customer WHERE COALESCE(referee_id, 0) != 2;
+
