da8a20854bdc77babd53edc5b798389ae146d279
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-16 11:23:39 +0600

    Update 0023_VIEW.sql

--- Database/Structure Query Language/0023_VIEW.sql
+++ Database/Structure Query Language/0023_VIEW.sql
@@ -1,7 +1,22 @@
--- https://www.geeksforgeeks.org/sql-views/
+-- carefull VIEW change the original as data -> https://stackoverflow.com/a/2648600/10928027
 
--- VIEW used for creating a seperate table for hiding complexity and unnessary information
+
+-- create VIEW from single table.
 CREATE VIEW Country AS SELECT countries.name FROM countries;
-
+-- create view from multiple tables.
 CREATE VIEW Country_on_region AS SELECT countries.name, regions.name AS region FROM countries JOIN regions ON countries.region_id = regions.region_id
 ORDER BY countries.name;
+
+-- update VIEW
+UPDATE Student_name
+SET Name='Adib'
+WHERE Name='Abid';
+
+
+-- I can't able to insert data in a VIEW, as there has some condition to meet before change or insertion in VIEW
+-- https://www.geeksforgeeks.org/sql-views/
+
+
+
+-- delete a VIEW
+DROP VIEW Student_name;
