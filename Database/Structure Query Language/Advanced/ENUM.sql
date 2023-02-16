569e29ab202a102db050797df97177070c260974
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-16 19:00:44 +0600

    Update ENUM.sql

--- Database/Structure Query Language/Advanced/ENUM.sql
+++ Database/Structure Query Language/Advanced/ENUM.sql
@@ -1,3 +1,6 @@
+-- https://www.geeksforgeeks.org/enumerator-enum-in-mysql/
+
+
 CREATE TABLE EnumTest(
     ID int(5) PRIMARY KEY AUTO_INCREMENT,
     CGPA float(3, 2),
@@ -6,3 +9,6 @@ CREATE TABLE EnumTest(
 
 INSERT INTO EnumTest
 VALUES(1, 2.58, 'Medium');
+
+INSERT INTO EnumTest
+VALUES(7, 2.45, 1);
