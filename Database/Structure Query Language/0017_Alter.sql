cc79616d0f27df75372b01a2936665b8d37e640f
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-02-17 09:42:09 +0600

    Update 0017_Alter.sql

--- Database/Structure Query Language/0017_Alter.sql
+++ Database/Structure Query Language/0017_Alter.sql
@@ -1,8 +1,13 @@
 -- add a new column
 ALTER TABLE Student ADD GPA float(3, 2);
 
--- change column name
+-- change column name (change column only for changing the name of column)
 ALTER TABLE Student CHANGE GPA CGPA float(5, 4);
+
+-- modify for increase or decrease the column size, change constrints etc
+ALTER TABLE UIU MODIFY Name varchar(25);
+
+-- change vs modify -> https://stackoverflow.com/a/14767467
 
 -- delete a column
 ALTER TABLE Student DROP COLUMN CGPA;
