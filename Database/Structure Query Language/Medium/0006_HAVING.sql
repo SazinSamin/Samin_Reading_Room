49de4f76b841ea2dd9ecf8bc36e2e0b096fae134
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-03-02 17:50:17 +0600

    Update 0006_HAVING.sql

--- Database/Structure Query Language/Medium/0006_HAVING.sql
+++ Database/Structure Query Language/Medium/0006_HAVING.sql
@@ -1,2 +1,3 @@
+-- https://www.w3schools.com/sql/sql_having.asp
 -- in case of aggregate function we can't use WHERE, we have to use HAVING.
 SELECT region_id, COUNT(region_id) FROM countries GROUP BY region_id HAVING COUNT(region_id) > 5; 
