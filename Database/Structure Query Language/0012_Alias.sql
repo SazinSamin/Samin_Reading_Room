fd06c1aef66d9bd2b26c4bd8a28fcb7b03cfae6b
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-03-02 10:34:42 +0600

    Update and rename 0012_As.sql to 0012_Alias.sql

---
+++ Database/Structure Query Language/0012_Alias.sql
@@ -0,0 +1,5 @@
+-- Alias of column name
+SELECT name AS 'Country Name', area AS 'Area(km^2)' FROM countries;
+
+-- Alias of table name
+SELECT * FROM countries c;
--- Database/Structure Query Language/0012_As.sql
+++
@@ -1,1 +0,0 @@
-SELECT name AS 'Country Name', area AS 'Area(km^2)' FROM countries;
