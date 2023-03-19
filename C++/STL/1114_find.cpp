588778d2c844235811d25ccd6bad06658b2bc3e3
SazinSamin <sazinsamin50@gmail.com>
2023-03-19 11:21:19 +0600

    add find in c++

---
+++ C++/STL/1114_find.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+        vector<int> v1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        vector<int>::iterator it;
+        it = find(v1.begin(), v1.end(), 2);
+        cout << *it << endl;
+
+
+}
