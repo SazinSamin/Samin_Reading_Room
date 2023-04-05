1841e4cc1addc2133f81ac868867f277962de9ee
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-04-05 11:56:07 +0600

    Update example1.cpp

--- Algorithm/Backtracking/Permutation/example1.cpp
+++ Algorithm/Backtracking/Permutation/example1.cpp
@@ -1,41 +1,33 @@
-// https://www.techiedelight.com/find-permutations-given-string/
+// https://www.youtube.com/watch?v=28okqq8raXM
+// permuation for all element in array is unique
 
-#include<iostream>
-#include<vector>
+
+#include<bits/stdc++.h>
 using namespace std;
 
+vector<int> res;
 
-
-void printVector(vector<int> arr) {
-    for(auto it = arr.begin(); it != arr.end(); it++) {
-        cout << *it << " ";
-    }
-    cout << endl;
+void print(vector<int>& nums) {
+        for(auto i : nums) {
+                cout << i << " ";
+        }
+        cout << "\n";
 }
 
-
-
-void permutation(vector<int> arr, int i, int n) {
-    // 
-    if(i == n - 1) {
-         printVector(arr);
-         return; 
-    }
-
-
-    // process each character of remaining string
-    for(int j = i; j < n; j++) {
-        // swap other character with current character
-        swap(arr[i], arr[j]);
-        // go for next character
-        permutation(arr, i+1, n);
-        // swap back to get the original string back
-        swap(arr[i], arr[j]);
-    }
+void permutation(vector<int>& nums, int idx) {
+        if(idx == nums.size() - 1) {
+                print(nums);
+                return;
+        }
+        for(int i = idx; i < nums.size(); i++) {
+                swap(nums[idx], nums[i]);
+                permutation(nums, idx + 1);
+                swap(nums[idx], nums[i]);
+        }
 }
 
 
 int main() {
-    vector<int> arr = {1, 2, 3};
-    permutation(arr, 0, 3);
+        vector<int> nums = {1, 2, 3, 4};
+        permutation(nums, 0);
 }
