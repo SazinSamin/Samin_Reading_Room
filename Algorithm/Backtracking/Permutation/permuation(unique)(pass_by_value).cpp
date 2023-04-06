e2b16b21c3ea5b2f298e5eb76f32c241511d48ff
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-04-06 11:14:01 +0600

    Create permuation(unique)(pass_by_value).cpp

---
+++ Algorithm/Backtracking/Permutation/permuation(unique)(pass_by_value).cpp
@@ -0,0 +1,29 @@
+// The 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<int>& nums) {
+        for(auto i : nums) {
+                cout << i << " ";
+        }
+        cout << "\n";
+}
+
+void permutation(vector<int> nums, int idx) {
+        if(idx == nums.size() - 1) {
+                print(nums);
+                return;
+        }
+        for(int i = idx; i < nums.size(); i++) {
+                swap(nums[idx], nums[i]);
+                permutation(nums, idx + 1);
+                // swap(nums[idx], nums[i]);
+        }
+}
+
+
+int main() {
+        vector<int> nums = {1, 2, 3};
+        permutation(nums, 0);
+}
